0b30e150657629acdd7297cb2cebb9cac104b8ff
Matthias Link <matthias@link.co.at>
2012-12-19 11:47:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapphire::Application.config.secret_token = '59e2635ad98d0cad43a93bc130e9eb6610918fd7c75db5c78854b9f6facde88b6ce171eed9a329422ade6a1e718870312d3322e7a81a16fab68980c7e481cd51'

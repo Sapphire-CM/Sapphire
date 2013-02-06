68b9b62bbc09239ce7425f6882e6c1cd1445d647
Matthias Link <matthias@link.co.at>
2013-02-06 18:00:52 +0100

    finalized method term_context?

--- app/helpers/term_resource_helper.rb
+++ app/helpers/term_resource_helper.rb
@@ -1,2 +1,14 @@
 module TermResourceHelper
+  def term_context?
+    if @in_term_context.nil?
+      @in_term_context = current_controller.is_a? TermResourceController
+    end
+    
+    @in_term_context
+    false
+  end
+  
+  def current_controller
+    (params[:controller].capitalize + "Controller").constantize
+  end
 end

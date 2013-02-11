f7b8b346639ff48d866d126505512064fcf5becd
Matthias Link <matthias@link.co.at>
2013-02-11 15:21:42 +0100

    added forename and surname to tutors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211115609) do
+ActiveRecord::Schema.define(:version => 20130211140604) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -148,6 +148,8 @@ ActiveRecord::Schema.define(:version => 20130211115609) do
   create_table "tutors", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "forename"
+    t.string   "surname"
   end
 
 end

27041802854de9338b2955fec4f069a83749d271
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-05-03 23:30:45 +0200

    fixed rating group title generation with more helper methods

--- app/helpers/rating_groups_helper.rb
+++ app/helpers/rating_groups_helper.rb
@@ -1,8 +1,2 @@
 module RatingGroupsHelper
-
-  def subtitle_points(rating_group)
-    subtitle = "#{pluralize rating_group.points, "point"}"
-    subtitle += " (#{rating_group.min_points} ... #{rating_group.max_points})" if rating_group.enable_range_points
-  end
-
 end

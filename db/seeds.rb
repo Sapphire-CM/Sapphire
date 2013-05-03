ffd6297808fbb60596cc1649073b5f82ed802c06
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-05-03 23:06:39 +0200

    added seeds for rating_groups of actual INM course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
 courses = Course.create([
   { title: 'INM' },
   { title: 'HCI' },
-  { title: 'Bakk-Proj' }])
+  { title: 'IAWEB' }])
 
 terms = Term.create([
-  { title: 'WS 2011', course: courses[0] },
-  { title: 'WS 2012', course: courses[0] },
   { title: 'WS 2013', course: courses[0] },
-  { title: 'SS 2011', course: courses[1] },
-  { title: 'SS 2012', course: courses[1] },
-  { title: 'SS 2013', course: courses[1] }])
+  { title: 'WS 2014', course: courses[0] },
+  { title: 'SS 2013', course: courses[1] },
+  { title: 'SS 2014', course: courses[1] },
+  { title: 'WS 2013', course: courses[2] } ])
 
 exercises = Exercise.create([
   { title: 'Ex 1: Newsgroup',  term: terms[0] },
@@ -17,15 +16,48 @@ exercises = Exercise.create([
   { title: 'Ex 3.1: Email',    term: terms[0] },
   { title: 'Ex 3.2: Research', term: terms[0] },
   { title: 'Ex 4: Website',    term: terms[0] },
-  { title: 'Ex 5: CSS',        term: terms[0] }])
+  { title: 'Ex 5: CSS',        term: terms[0] },
+  { title: 'MC Test',          term: terms[0] } ])
 
 rating_groups = RatingGroup.create([
-  { title: 'Identity',         points:  7, exercise: exercises[0] },
-  { title: 'Format',           points:  7, exercise: exercises[0] },
-  { title: 'Content',          points: 11, exercise: exercises[0] },
-  { title: 'General stuff',    points: 10, exercise: exercises[0], global: true },
-  { title: 'General stuff 2',  points: 11, exercise: exercises[0] },
-  { title: 'Miscellaneous',    points:  0, exercise: exercises[0] }])
+  { title: 'Identity',                points:   7, exercise: exercises[0] },
+  { title: 'Format',                  points:   7, exercise: exercises[0] },
+  { title: 'Content',                 points:  11, exercise: exercises[0] },
+  { title: 'General',                 points: nil, exercise: exercises[0], global: true },
+
+  { title: 'Identity',                points:   7, exercise: exercises[1] },
+  { title: 'Format',                  points:   7, exercise: exercises[1] },
+  { title: 'Quoting',                 points:  10, exercise: exercises[1] },
+  { title: 'Content',                 points:   6, exercise: exercises[1] },
+  { title: 'General',                 points: nil, exercise: exercises[1], global: true },
+
+  { title: 'Identity & Format',       points:  15, exercise: exercises[2] },
+  { title: 'General',                 points: nil, exercise: exercises[2], global: true },
+
+  { title: 'PDF',                     points:   5, exercise: exercises[3] },
+  { title: 'Person',                  points:   3, exercise: exercises[3] },
+  { title: 'Publication',             points:  10, exercise: exercises[3] },
+  { title: 'Summary',                 points:  10, exercise: exercises[3] },
+  { title: 'General',                 points: nil, exercise: exercises[3], global: true },
+
+  { title: 'Files & Directories',     points:   7, exercise: exercises[4] },
+  { title: 'Index.xhtml',             points:   7, exercise: exercises[4] },
+  { title: 'Posting.xhtml',           points:   7, exercise: exercises[4] },
+  { title: 'Person.xhtml',            points:   7, exercise: exercises[4] },
+  { title: 'XHTML elements',          points:   7, exercise: exercises[4] },
+  { title: 'Image (anywhere)',        points:   7, exercise: exercises[4] },
+  { title: 'inm.css',                 points:   7, exercise: exercises[4] },
+  { title: 'XHTML coding (anywhere)', points:   7, exercise: exercises[4] },
+  { title: 'General',                 points: nil, exercise: exercises[4], global: true },
+
+  { title: 'Email format',            points:  10, exercise: exercises[5] },
+  { title: 'graztimes.css',           points:  10, exercise: exercises[5] },
+  { title: 'print.css',               points:  10, exercise: exercises[5] },
+  { title: 'kids.css',                points:  10, exercise: exercises[5] },
+  { title: 'General',                 points: nil, exercise: exercises[5], global: true },
+
+  { title: 'Questions',               points:  40, exercise: exercises[6] },
+  { title: 'General',                 points: nil, exercise: exercises[6], global: true } ])
 
 binary_number_ratings = BinaryNumberRating.create([
   { title: 'no realname',                        value: -3, rating_group: rating_groups[0] },

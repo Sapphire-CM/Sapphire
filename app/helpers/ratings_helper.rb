ecb6356cdbcede906d61a64c78d785ef288c4fcc
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-05-18 21:26:43 +0200

    implemented ratings management with ajax calls inline in exercise-show

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -1,8 +1,4 @@
 module RatingsHelper
-  def link_to_rating(title, rating, options = {})
-    link_to title, course_term_exercise_rating_group_rating_path(current_course, current_term, rating.exercise, rating.rating_group, rating), options
-  end
-  
   def ratings_form_collection
     [['Boolean Points', BinaryNumberRating.name],['Boolean Percentage', BinaryPercentRating.name], ['Min/Max Points', ValueNumberRating.name], ['Min/Max Percentage', ValuePercentRating.name]]
   end

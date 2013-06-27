4b675f68ad2be94164b119d3d774cb1636b3d8f8
Matthias Link <matthias@link.co.at>
2013-06-27 12:39:42 +0200

    fixed initialization of bullet

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,3 +1,5 @@
-Bullet.enable = true
-Bullet.bullet_logger = true
-# Bullet.alert = true
+if defined?(Bullet) && Rails.env == :development
+  Bullet.enable = true
+  Bullet.bullet_logger = true
+  # Bullet.alert = true
+end

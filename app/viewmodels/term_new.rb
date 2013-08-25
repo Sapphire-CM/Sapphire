0cac2fbd73aecd98dc9cfb825f06856a7a8ff763
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-08-25 21:16:13 +0200

    simplified term_new viewmodel

--- app/viewmodels/term_new.rb
+++ app/viewmodels/term_new.rb
@@ -1,38 +1,6 @@
 class TermNew < Term
 
   attr_accessible :copy_elements, :source_term_id, :copy_lecturer, :copy_exercises
-
-  def copy_elements
-    @copy_elements ||= nil
-  end
-
-  def source_term_id
-    @source_term_id ||= nil
-  end
-
-  def copy_lecturer
-    @copy_lecturer ||= nil
-  end
-
-  def copy_exercises
-    @copy_exercises ||= nil
-  end
-
-
-  def copy_elements=(value)
-    @copy_elements = value
-  end
-
-  def source_term_id=(value)
-    @source_term_id = value
-  end
-
-  def copy_lecturer=(value)
-    @copy_lecturer = value
-  end
-
-  def copy_exercises=(value)
-    @copy_exercises = value
-  end
+  attr_accessor :copy_elements, :source_term_id, :copy_lecturer, :copy_exercises
 
 end

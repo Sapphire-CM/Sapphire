1fa2e0f9285e9036658009a8811abbce222814fa
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-08-25 22:24:38 +0200

    automatically create all StudentGroupRegistrations for all non-group_submission exercises, improved import smart guessing

--- app/models/student_group_registration.rb
+++ app/models/student_group_registration.rb
@@ -2,6 +2,8 @@ class StudentGroupRegistration < ActiveRecord::Base
   belongs_to :student_group
   belongs_to :exercise
 
+  attr_accessible :student_group, :exercise, :student_group_id
+
   has_many :submissions
 
 end
--- lib/import/importer.rb
+++ lib/import/importer.rb
@@ -8,12 +8,27 @@ module Import::Importer
       row = values.first
 
       row.each_index do |cell_index|
-        smart_guessed_import_mapping.group = cell_index if /\A(T|G)[\d]{1}/ =~ row[cell_index]
-        smart_guessed_import_mapping.email = cell_index if /.*?@?.*/ =~ row[cell_index]
-        smart_guessed_import_mapping.matriculum_number = cell_index if /\A[\d]{7}\z/ =~ row[cell_index]
+        if /\A(T|G)[\d]{1}/ =~ row[cell_index]
+          smart_guessed_import_mapping.group ||= cell_index
+          next
+        end
+
+        if /.+@.+\..+/ =~ row[cell_index]
+          smart_guessed_import_mapping.email ||= cell_index
+          next
+        end
+
+        if /\A[\d]{7}\z/ =~ row[cell_index]
+          smart_guessed_import_mapping.matriculum_number ||= cell_index
+          next
+        end
 
         begin
-          smart_guessed_import_mapping.registered_at = cell_index if row[cell_index].to_datetime
+          datetime = row[cell_index].to_datetime
+          if not datetime.blank?
+            smart_guessed_import_mapping.registered_at ||= cell_index
+            next
+          end
         rescue
         end
       end
@@ -21,9 +36,20 @@ module Import::Importer
 
     # hard coded values of headers
     headers.each_index do |cell_index|
-      smart_guessed_import_mapping.forename = cell_index if /.*Vorname.*/ =~ headers[cell_index]
-      smart_guessed_import_mapping.surname = cell_index if /.*Nachname.*/ =~ headers[cell_index]
-      smart_guessed_import_mapping.comment = cell_index if /.*Anmerkung.*/ =~ headers[cell_index]
+      if /.*Vorname.*/ =~ headers[cell_index]
+        smart_guessed_import_mapping.forename ||= cell_index
+        next
+      end
+
+      if /.*Nachname.*/ =~ headers[cell_index]
+        smart_guessed_import_mapping.surname ||= cell_index
+        next
+      end
+
+      if /.*Anmerkung.*/ =~ headers[cell_index]
+        smart_guessed_import_mapping.comment ||= cell_index
+        next
+      end
     end if headers
 
     self.import_mapping = smart_guessed_import_mapping
@@ -49,7 +75,6 @@ module Import::Importer
       values.each do |row|
         student = create_student_account row
 
-
         group_title = row[import_mapping.group.to_i]
 
         case import_options[:matching_groups]
@@ -71,6 +96,7 @@ module Import::Importer
         tutorial_group = create_tutorial_group "T#{tutorial_group_title}"
         student_group = create_student_group "G#{student_group_title}", tutorial_group
         create_student_registration row, student, student_group, tutorial_group
+        create_student_group_registrations student_group
       end
 
       self.status = "imported"
@@ -89,12 +115,10 @@ module Import::Importer
   end
 
 
-
-
 private
 
   def create_student_account(row)
-    student = Account.find_or_initialize_by_matriculum_number(row[import_mapping.matriculum_number.to_i])
+    student = Account.find_or_initialize_by(matriculum_number: row[import_mapping.matriculum_number.to_i])
     student.forename = row[import_mapping.forename.to_i]
     student.surname = row[import_mapping.surname.to_i]
     student.email = row[import_mapping.email.to_i]
@@ -115,7 +139,7 @@ private
   end
 
   def create_tutorial_group(title)
-    tutorial_group = term.tutorial_groups.find_or_initialize_by_title(title)
+    tutorial_group = term.tutorial_groups.find_or_initialize_by(title: title)
 
     new_record = tutorial_group.new_record?
     if tutorial_group.save
@@ -129,7 +153,7 @@ private
   end
 
   def create_student_group(title, tutorial_group)
-    student_group = tutorial_group.student_groups.find_or_initialize_by_title(title)
+    student_group = tutorial_group.student_groups.find_or_initialize_by(title: title)
 
     new_record = student_group.new_record?
     if student_group.save
@@ -145,7 +169,7 @@ private
   def create_student_registration(row, student, student_group, tutorial_group)
     all_students = tutorial_group.student_groups.flat_map{ |sg| sg.students }
     if !all_students.empty? && all_students.include?(student)
-      registration = student_group.student_registrations.find_or_initialize_by_account_id(student.id)
+      registration = student_group.student_registrations.find_or_initialize_by(account_id: student.id)
     else
       registration = student_group.student_registrations.new
       registration.student = student
@@ -165,6 +189,13 @@ private
     registration
   end
 
+  def create_student_group_registrations(student_group)
+    term.exercises.where(group_submission: false).each do |exercise|
+      student_group_registration = StudentGroupRegistration.find_or_initialize_by(student_group_id: student_group.id)
+      student_group_registration.exercise = exercise
+      student_group_registration.save
+    end
+  end
 
   def create_problem_definition(row, full_messages)
     entry = {

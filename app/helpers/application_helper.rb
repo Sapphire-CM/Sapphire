50869eb9d6894b0408b6eaf612766fa3b9978168
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-09-24 11:52:46 +0200

    fixed wrong context detection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def term_context?
+    if @term_context.nil?
+      @term_context = (current_controller != StaticController &&
+        current_controller != AccountsController &&
+        current_controller != CoursesController &&
+        current_controller != TermsController ) ||
+      (params[:controller] == "terms" && params[:action] == "show") || (params[:term_id])
+    end
+
+    @term_context
+  end
+
+  def current_controller
+    (params[:controller]+"_controller").camelize.constantize
+  end
 end

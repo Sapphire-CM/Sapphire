5f4b3df6b7ce37c7e8aefb6d10a00006cb526b30
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-09-24 20:40:09 +0200

    fix faulty migration

--- db/migrate/20121222202211_rename_semester_to_term.rb
+++ db/migrate/20121222202211_rename_semester_to_term.rb
@@ -1,7 +1,7 @@
 class RenameSemesterToTerm < ActiveRecord::Migration
   def change
+    rename_index :semesters, "index_semesters_on_course_id", "index_terms_on_course_id"
     rename_table :semesters, :terms
     rename_column :import_student_imports, :semester_id, :term_id
-    rename_index :terms, "index_semesters_on_course_id", "index_terms_on_course_id"
   end
 end

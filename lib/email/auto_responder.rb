7fb1069146f47f7da2489c8d935306c520a3dacb
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-11-15 20:26:28 +0100

    fixed wrong method invocation

--- lib/email/auto_responder.rb
+++ lib/email/auto_responder.rb
@@ -17,7 +17,7 @@ def deliver_mail(args)
     reply_to args[:reply_to]
     subject args[:subject]
 
-    charset 'UTF-8'
+    charset = 'UTF-8'
     content_type 'text/plain; charset=UTF-8'
     transport_encoding '8bit'
 

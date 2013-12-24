0af63359ee63181a6c9b08ce0067292a190294a7
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2013-12-24 11:17:31 +0100

    updated deployment strategy

--- Capfile
+++ Capfile
@@ -1,9 +1,55 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
-require 'capistrano/bundler'
-require 'capistrano/rails'
 require 'capistrano/rvm'
+require 'capistrano/rails'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+  after :publishing, 'deploy:restart'
+
+  after :finishing, 'deploy:cleanup'
+
+  namespace :db do
+    desc 'Drops the database to an empty state'
+    task :drop do
+      on primary :db do
+        within release_path do
+          with rails_env: fetch(:stage) do
+            execute :rake, "db:drop"
+          end
+        end
+      end
+    end
+
+    desc 'Resets the database to an empty state'
+    task :reset do
+      on primary :db do
+        within release_path do
+          with rails_env: fetch(:stage) do
+            execute :rake, "db:reset"
+          end
+        end
+      end
+    end
+
+    desc "Load the seed data from db/seeds.rb"
+    task :seed do
+      on primary :db do
+        within release_path do
+          with rails_env: fetch(:stage) do
+            execute :rake, "db:seed"
+          end
+        end
+      end
+    end
+  end
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 set :application, 'sapphire'
+set :deploy_to, "/home/sapphire/#{fetch(:application)}"
+
 set :repo_url, 'git@github.com:matthee/Sapphire.git'
 set :scm, :git
-
-set :deploy_to, "/home/sapphire/#{fetch(:application)}"
-set :deploy_via, :copy
 
 set :stages, %w(staging production)
 
@@ -11,66 +10,3 @@ set :linked_files, %w{config/database.yml config/mail.yml}
 set :linked_dirs, %w{bin log emails uploads persistent}
 
 set :log_level, :debug
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
-  namespace :db do
-    desc 'Drops the database to an empty state'
-    task :drop do
-      on primary :db do
-        within release_path do
-          with rails_env: fetch(:stage) do
-            execute :rake, "db:drop"
-          end
-        end
-      end
-    end
-    before :drop, 'rvm:hook'
-
-    desc 'Resets the database to an empty state'
-    task :reset do
-      on primary :db do
-        within release_path do
-          with rails_env: fetch(:stage) do
-            execute :rake, "db:reset"
-          end
-        end
-      end
-    end
-    before :reset, 'rvm:hook'
-
-    desc "Load the seed data from db/seeds.rb"
-    task :seed do
-      on primary :db do
-        within release_path do
-          with rails_env: fetch(:stage) do
-            execute :rake, "db:seed"
-          end
-        end
-      end
-    end
-    before :seed, 'rvm:hook'
-
-    desc "Updates the secret_key_base for cookies"
-    task :seed do
-      on primary :db do
-        within release_path do
-          with rails_env: fetch(:stage) do
-            execute :rake, "secret:update"
-          end
-        end
-      end
-    end
-    before :seed, 'rvm:hook'
-
-  end
-end

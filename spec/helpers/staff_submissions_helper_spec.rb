60dc5590c3232dcf7f149ae6781dbfabd279b286
Matthias Link <matthias@link.co.at>
2014-08-21 16:27:30 +0200

    UI Changes: minor changes implementing Keith's recommendations

--- spec/helpers/staff_submissions_helper_spec.rb
+++ spec/helpers/staff_submissions_helper_spec.rb
@@ -15,7 +15,7 @@ describe StaffSubmissionsHelper do
     it "should generate the appropriate title for a submission with a student group" do
       submission = FactoryGirl.create(:submission, :with_student_group_registration, student_group_title: "Group 01")
 
-      subject.staff_submission_title(submission).should eq("Submission of Group 01")
+      helper.staff_submission_title(submission).should eq("Submission of Group 01")
     end
 
     it "should generate the appropriate title for a submission without a student group" do
@@ -25,8 +25,8 @@ describe StaffSubmissionsHelper do
       solitary_submission = FactoryGirl.create(:submission, exercise: solitary_exercise)
       group_submission = FactoryGirl.create(:submission, exercise: group_exercise)
 
-      subject.staff_submission_title(solitary_submission).should eq("Submission of unknown student")
-      subject.staff_submission_title(group_submission).should eq("Submission of unknown group")
+      helper.staff_submission_title(solitary_submission).should eq("Submission of unknown student")
+      helper.staff_submission_title(group_submission).should eq("Submission of unknown group")
     end
   end
 end

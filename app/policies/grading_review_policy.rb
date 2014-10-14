892b664369bbbddf438b3bd7b91192c319e347c5
Matthias Link <matthias@link.co.at>
2014-10-14 14:10:58 +0200

    Policies: using new method for determination if tutor of term

--- app/policies/grading_review_policy.rb
+++ app/policies/grading_review_policy.rb
@@ -2,12 +2,12 @@ class GradingReviewPolicy < PunditBasePolicy
   def index?
     user.admin? ||
     user.lecturer_of_term?(record.term) ||
-    user.tutor_of_any_tutorial_group_in_term?(record.term)
+    user.tutor_of_term?(record.term)
   end
 
   def show?
     user.admin? ||
     user.lecturer_of_term?(record.term) ||
-    user.tutor_of_any_tutorial_group_in_term?(record.term)
+    user.tutor_of_term?(record.term)
   end
 end
--- app/policies/tutorial_group_policy.rb
+++ app/policies/tutorial_group_policy.rb
@@ -3,14 +3,14 @@ class TutorialGroupPolicy < PunditBasePolicy
     user.admin? ||
     user.lecturer_of_term?(record.subject) ||
     user.tutor_of_term?(record.subject) ||
-    user.tutor_of_any_tutorial_group_in_term?(record.subject)
+    user.tutor_of_term?(record.subject)
   end
 
   def show?
     user.admin? ||
     user.lecturer_of_term?(record.term) ||
     user.tutor_of_term?(record) ||
-    user.tutor_of_any_tutorial_group_in_term?(record.term)
+    user.tutor_of_term?(record.term)
   end
 
   def new?
@@ -42,6 +42,6 @@ class TutorialGroupPolicy < PunditBasePolicy
     user.admin? ||
     user.lecturer_of_term?(record.term) ||
     user.tutor_of_term?(record) ||
-    user.tutor_of_any_tutorial_group_in_term?(record.term)
+    user.tutor_of_term?(record.term)
   end
 end

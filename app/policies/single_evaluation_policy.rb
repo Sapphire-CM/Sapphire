b83350df52e560293c826d57014e1e1bae4abac9
Matthias Link <matthias@link.co.at>
2014-10-14 16:22:28 +0200

    Pundit: allowing access to single evaluations to all tutors of the term

--- app/policies/single_evaluation_policy.rb
+++ app/policies/single_evaluation_policy.rb
@@ -2,12 +2,12 @@ class SingleEvaluationPolicy < PunditBasePolicy
   def show?
     user.admin? ||
     user.lecturer_of_term?(record.submission.exercise.term) ||
-    user.tutor_registrations.where(tutorial_group_id: record.submission.term_registrations.map(&:tutorial_group_id)).exists?
+    user.tutor_of_term?(record.submission.exercise.term)
   end
 
   def update?
     user.admin? ||
     user.lecturer_of_term?(record.submission.exercise.term) ||
-    user.tutor_registrations.where(tutorial_group_id: record.submission.term_registrations.map(&:tutorial_group_id)).exists?
+    user.tutor_of_term?(record.submission.exercise.term)
   end
 end

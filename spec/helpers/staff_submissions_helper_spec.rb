853307ad26ce266cae500b80e2adfa41b6062c79
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-10-26 11:21:44 +0000

    remove empty specs

--- spec/helpers/staff_submissions_helper_spec.rb
+++ spec/helpers/staff_submissions_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the StaffSubmissionsHelper. For example:
-#
-# describe StaffSubmissionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe StaffSubmissionsHelper do
   describe "staff_submission_title" do
     it "should generate the appropriate title for a submission with a student group" do

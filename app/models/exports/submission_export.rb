9d787541f92608327a0da97ac14f14e3153f671f
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-11-08 12:13:48 +0000

    fix export zip command
    use quotes around path/filenames
    create at least one file for the zip command to work with

--- app/models/exports/submission_export.rb
+++ app/models/exports/submission_export.rb
@@ -33,8 +33,11 @@ class SubmissionExport < Export
     zip_filename = File.join(zips_path, "#{term.course.title.parameterize}-#{term.title.parameterize}-#{self.id}.zip")
     FileUtils.rm(zip_filename) if File.exist? zip_filename
 
-    cmd = "cd #{directory} && zip -r #{zip_filename} *"
+    # this ensures that there is at least one file to create a zip for
+    # otherwise the zip command fails and therefore this export fails
+    File.write(File.join(directory, 'export.txt'), "Export created on #{Time.now}")
 
+    cmd = %Q{cd "#{directory}" && zip -r "#{zip_filename}" *}
     `#{cmd}`
 
     self.file = File.open(zip_filename)

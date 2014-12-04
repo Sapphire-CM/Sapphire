46abfc6215402a13dadd986b42321d5b87a76d58
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-04 18:08:30 +0000

    fix evaluation_group associations

--- spec/factories/evaluation_group_factory.rb
+++ spec/factories/evaluation_group_factory.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :evaluation_group do
+    rating_group
+    submission_evaluation nil
+
     points 1
     percent 1.5
-    rating_group nil
-    submission_evaluation nil
   end
 end

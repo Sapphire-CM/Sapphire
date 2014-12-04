21e3214f1b1834aa2e41a8bca5547e12d367ebc7
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-04 17:35:26 +0000

    update pundit

--- spec/support/pundit_helpers.rb
+++ spec/support/pundit_helpers.rb
@@ -1,3 +1,6 @@
+# alternative pundit & rspec integration
+# http://thunderboltlabs.com/blog/2013/03/27/testing-pundit-policies-with-rspec/
+
 RSpec::Matchers.define :permit_authorization do |action|
   match do |policy|
     policy.public_send("#{action}?")

387fa54edd9d19174217bf9a65c16de34cfca892
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-04 12:23:00 +0000

    remove empty spec files

--- spec/controllers/single_evaluations_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe SingleEvaluationsController do
-
-end
--- spec/models/export_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe Export do
-  pending "add some examples to (or delete) #{__FILE__}"
-end

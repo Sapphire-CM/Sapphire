5463db0566adea35c3cf1966c3349d79b1841463
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-04 11:20:48 +0000

    cleanup simplecov config

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,9 +1,2 @@
 require 'simplecov'
-
-SimpleCov.start 'rails' do
-  add_group "Policies", "app/policies"
-  add_group "Decorators", "app/decorators"
-  add_group "Uploaders", "app/uploaders"
-end
-
 SimpleCov.command_name 'RSpec'

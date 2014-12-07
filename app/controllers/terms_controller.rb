f966933477fb5bee2552968958cb98b31a9c2940
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-07 16:17:35 +0000

    add terms controller spec

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,7 +1,6 @@
 class TermsController < ApplicationController
   before_action :set_term, only: [:show, :edit, :update, :destroy,
-    :new_lecturer_registration, :create_lecturer_registration, :clear_lecturer_registration,
-    :update_grading_scale, :points_overview]
+    :points_overview]
 
   def show
     @tutorial_groups = @term.tutorial_groups
@@ -56,32 +55,12 @@ class TermsController < ApplicationController
     redirect_to root_path
   end
 
-  def new_lecturer_registration
-    @accounts = Account.scoped.page(params[:page]).per(50)
-    @accounts = @accounts.search(params[:q]) if params[:q].present?
-  end
-
-  def create_lecturer_registration
-    registration = LecturerRegistration.find_or_initialize_by(term_id: @term.id)
-    registration.lecturer = Account.find(params[:account_id])
-
-    if registration.save
-      redirect_to @term, notice: "Lecturer registration successfully added."
-    else
-      redirect_to @term, alert: "Lecturer registration failed!"
-    end
-  end
-
-  def clear_lecturer_registration
-    @term.lecturer_registration.destroy
-    redirect_to @term, notice: "Lecturer registration successfully cleared!"
-  end
-
   def points_overview
     @grading_scale = GradingScaleService.new(@term)
   end
 
   private
+
   def set_term
     @term = Term.find(params[:id] || params[:term_id])
     authorize @term
@@ -96,6 +75,7 @@ class TermsController < ApplicationController
       :copy_elements,
       :copy_exercises,
       :copy_grading_scale,
-      :copy_lecturer)
+      :copy_lecturer
+    )
   end
 end

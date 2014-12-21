62cf32588b0db7381648943a165872ddfb425de7
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-21 12:11:01 +0000

    update to Rails 4.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,14 @@ Sapphire::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Sapphire::Application.config.session_store :cookie_store, key: '_sapphire_session'
+Rails.application.config.session_store :cookie_store, key: '_sapphire_session'

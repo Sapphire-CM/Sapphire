fb04b695a97749ed7ed8cc07bfd6d3593449fc93
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-22 18:59:36 +0000

    do not link bin directory on deployment
    use the binstubs directly from the repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
 set :stages, %w(staging production)
 
 set :linked_files, %w{config/database.yml config/mail.yml}
-set :linked_dirs, %w{bin log emails uploads persistent tmp/pids}
+set :linked_dirs, %w{log emails uploads persistent tmp/pids}
 
 set :log_level, :debug
 

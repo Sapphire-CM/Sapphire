9d85f5425a3e3d0c0989de0c507944ddc39a219c
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-26 16:43:54 +0000

    use proper private-modifier for controller methods

--- app/controllers/concerns/exercise_context.rb
+++ app/controllers/concerns/exercise_context.rb
@@ -1,19 +1,20 @@
 module ExerciseContext
   extend ActiveSupport::Concern
 
-  def current_exercise
-    @exercise
-  end
-
-  private
-  def fetch_exercise
-    @exercise = current_term.exercises.find(params[:exercise_id])
-  end
-
   included do
     include TermContext
 
     before_action :fetch_exercise
     helper_method :current_exercise
+  end
+
+  private
+
+  def current_exercise
+    @exercise
+  end
+
+  def fetch_exercise
+    @exercise = current_term.exercises.find(params[:exercise_id])
   end
 end

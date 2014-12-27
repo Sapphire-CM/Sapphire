a0296f6ee358ce1f3dcc732387b15100deaaa3b9
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-27 22:33:22 +0000

    implemented staff_submissions controller spec

--- app/controllers/staff_submissions_controller.rb
+++ app/controllers/staff_submissions_controller.rb
@@ -1,10 +1,8 @@
 class StaffSubmissionsController < ApplicationController
   include ScopingHelpers
 
-  before_action :set_exercise_and_term
-  before_action :set_tutorial_group
-  before_action :set_submissions, only: :index
-  before_action :set_submission, only: [:show, :update]
+  before_action :set_context
+  before_action :set_submission, only: [:edit, :update, :destroy]
   before_action :set_student_groups, only: [:new, :create, :edit, :update]
 
   SubmissionPolicyRecord = Struct.new :exercise, :tutorial_group do
@@ -16,39 +14,38 @@ class StaffSubmissionsController < ApplicationController
   def index
     authorize SubmissionPolicyRecord.new @exercise, @tutorial_group
 
-    @submission_count = @submissions.count
+    @submissions = scoped_submissions(@tutorial_group, @exercise.submissions)
     @submissions = @submissions.includes({exercise_registrations: {term_registration: :account}}, :submission_evaluation, :exercise).load
-    @submissions = @submissions.page(params[:page]).per(20)
+    @submission_count = @submissions.count
+    @submissions = @submissions.page(params[:page])
   end
 
   def new
     @submission = @exercise.submissions.new
-    @submission.submitted_at = Time.now
     authorize @submission
+
     @submission.submission_assets.build
   end
 
   def create
     @submission = @exercise.submissions.new(submission_params)
     authorize @submission
+
     @submission.submitter = current_account
+    @submission.submitted_at = Time.now
 
     if @submission.save
       redirect_to [@exercise, @submission], notice: "Submission successfully created"
     else
+      @submission.submission_assets.build
       render :new
     end
   end
 
   def edit
-    @submission = @exercise.submissions.find(params[:id])
-    authorize @submission
   end
 
   def update
-    @submission = @exercise.submissions.find(params[:id])
-    authorize @submission
-
     if @submission.update(submission_params)
       redirect_to exercise_submission_path(@exercise, @submission), notice: "Submission successfully updated"
     else
@@ -57,21 +54,26 @@ class StaffSubmissionsController < ApplicationController
   end
 
   def destroy
-    @submission = @exercise.submissions.find(params[:id])
-    authorize @submission
     @submission.destroy
-
     redirect_to exercise_submissions_path(@exercise)
   end
 
   private
   def submission_params
-    params.require(:submission).permit(:submitted_at, :student_group_id, submission_assets_attributes: [:id, :file, :_destroy])
+    params.require(:submission).permit(
+      :submitted_at,
+      :student_group_id,
+      submission_assets_attributes: [
+        :id,
+        :file,
+        :_destroy
+      ])
   end
 
-  def set_exercise_and_term
+  def set_context
     @exercise = Exercise.find params[:exercise_id]
     @term = @exercise.term
+    @tutorial_group = current_tutorial_group(@term)
   end
 
   def set_submission
@@ -90,13 +92,5 @@ class StaffSubmissionsController < ApplicationController
       @student_groups.solitary
     end
     @student_groups
-  end
-
-  def set_tutorial_group
-    @tutorial_group = current_tutorial_group(@term)
-  end
-
-  def set_submissions
-    @submissions = scoped_submissions(@tutorial_group, @exercise.submissions)
   end
 end
--- spec/controllers/staff_submissions_controller_spec.rb
+++ spec/controllers/staff_submissions_controller_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe StaffSubmissionsController do
   let!(:term) { FactoryGirl.create :term }
   let!(:exercise) { FactoryGirl.create :exercise, term: term }
   let!(:tutorial_group) { FactoryGirl.create :tutorial_group, term: term }
+  let!(:submission) { FactoryGirl.create :submission, exercise: exercise }
+  let!(:submission_asset) { FactoryGirl.create :submission_asset, submission: submission }
 
   describe 'GET index' do
     context 'with tutorial_group' do
@@ -26,5 +28,100 @@ RSpec.describe StaffSubmissionsController do
     end
   end
 
-  it 'needs to be implemented'
+  describe 'GET new' do
+    it 'assigns a new submission as @submission' do
+      get :new, exercise_id: exercise.id
+
+      expect(response).to have_http_status(:success)
+      expect(assigns(:submission)).to be_a_new(Submission)
+      expect(assigns(:submission).submission_assets.size).to eq(1)
+    end
+  end
+
+  describe 'POST create' do
+    describe 'with valid params' do
+      context 'creates a new submission' do
+        (0..2).each do |assets_count|
+          it "with #{assets_count} submission_assets" do
+            valid_attributes = {
+              exercise_id: exercise.id,
+              submission: {
+                submission_assets_attributes: {}
+              }
+            }
+
+            (1..assets_count).each do |asset|
+              valid_attributes[:submission][:submission_assets_attributes]["#{asset}"] = {
+                file: Rack::Test::UploadedFile.new(FactoryGirl.create(:submission_asset).file.to_s, 'text/html')
+              }
+            end
+
+            expect {
+              expect {
+                post :create, valid_attributes
+              }.to change(SubmissionAsset, :count).by(assets_count)
+            }.to change(Submission, :count).by(1)
+
+            expect(response).to redirect_to(exercise_submission_path(exercise, assigns(:submission)))
+          end
+        end
+      end
+    end
+
+    describe 'with invalid params' do
+      # can not happen
+    end
+  end
+
+  describe 'GET edit' do
+    it 'assigns the requested submission as @submission' do
+      get :edit, exercise_id: exercise.id, id: submission.id
+
+      expect(response).to have_http_status(:success)
+      expect(assigns(:submission)).to eq(submission)
+    end
+  end
+
+  describe 'PUT update' do
+    describe 'with valid params' do
+      it 'updates the requested submission' do
+        valid_attributes = {
+          exercise_id: exercise.id,
+          id: submission.id,
+          submission: {
+            submission_assets_attributes: {
+              '0' => {
+                id: submission_asset.id,
+                file: Rack::Test::UploadedFile.new(FactoryGirl.create(:submission_asset).file.to_s, 'text/html')
+              }
+            }
+          }
+        }
+
+        expect {
+          expect {
+            post :update, valid_attributes
+          }.to change(SubmissionAsset, :count).by(0)
+        }.to change(Submission, :count).by(0)
+
+        expect(response).to redirect_to(exercise_submission_path(exercise, assigns(:submission)))
+      end
+    end
+
+    describe 'with invalid params' do
+      # can not happen
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it 'destroys the requested submission' do
+      submission.reload # trigger creation
+
+      expect {
+        delete :destroy, exercise_id: exercise.id, id: submission.id
+      }.to change(Submission, :count).by(-1)
+
+      expect(response).to redirect_to(exercise_submissions_path(exercise))
+    end
+  end
 end

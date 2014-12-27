507e0ba562c41d1c073e6e7f9e03834553617c83
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2014-12-27 12:34:01 +0000

    implement single_evaluation controller spec

--- app/controllers/single_evaluations_controller.rb
+++ app/controllers/single_evaluations_controller.rb
@@ -7,12 +7,12 @@ class SingleEvaluationsController < ApplicationController
     end
   end
 
-  before_action :set_submission_and_tutorial_group, only: :show
-
   def show
     @submission = Submission.find(params[:id])
     authorize SingleEvaluationPolicyRecord.new @submission
 
+    @term = @submission.exercise.term
+    @tutorial_group = current_tutorial_group(@term)
     @submission_assets = @submission.submission_assets.order(submitted_at: :desc)
 
     submission_scope = scoped_submissions(@tutorial_group, Submission.for_exercise(@submission.exercise))
@@ -21,8 +21,12 @@ class SingleEvaluationsController < ApplicationController
     @next_submission = submission_scope.previous(@submission, :submitted_at)
 
     @exercise = @submission.exercise
-    @evaluation_groups = @submission.submission_evaluation.evaluation_groups.includes([:rating_group, {evaluations: :rating}]).order{rating_group.ratings.row_order.asc}.order{rating_group.row_order.asc}
     @term = @exercise.term
+    @evaluation_groups = @submission.submission_evaluation.evaluation_groups
+      .includes([:rating_group, {evaluations: :rating}])
+      .order{rating_group.ratings.row_order.asc}
+      .order{rating_group.row_order.asc}
+      .references(:rating_group)
   end
 
   def update
@@ -35,19 +39,12 @@ class SingleEvaluationsController < ApplicationController
     @evaluation.value = if @rating.is_a? BinaryRating
       @evaluation.value == 1 ? 0 : 1
     else
-      params.require(:evaluation).permit(:value)[:value]
+      params[:evaluation][:value]
     end
     @evaluation.save!
 
     @submission_evaluation = @submission.submission_evaluation
     @submission_evaluation.evaluated_at = Time.now
     @submission_evaluation.save!
-  end
-
-  private
-  def set_submission_and_tutorial_group
-    @submission = Submission.find(params[:id])
-    @term = @submission.exercise.term
-    @tutorial_group = current_tutorial_group(@term)
   end
 end

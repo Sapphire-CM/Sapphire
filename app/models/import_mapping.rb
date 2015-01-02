5b1589e14150d9e582e6efb2abc011f51e4afad1
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2015-01-02 20:13:41 +0000

    remove unused method

--- app/models/import_mapping.rb
+++ app/models/import_mapping.rb
@@ -4,8 +4,4 @@ class ImportMapping < ActiveRecord::Base
   validates :import, presence: true
 
   IMPORTABLE = %i(group email forename surname matriculation_number comment)
-
-  def swapped
-    IMPORTABLE.map {|a| [send(a), a] }.to_h
-  end
 end

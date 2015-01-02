c6a2f63d24574aa7e8492b73be8d5e345c6fd9f1
Matthias Link <matthias@link.co.at>
2015-01-02 19:51:00 +0100

    Excel Spreadsheet Export: merged columns of grading scale

--- app/models/exports/excel_spreadsheet_export.rb
+++ app/models/exports/excel_spreadsheet_export.rb
@@ -146,7 +146,7 @@ class ExcelSpreadsheetExport < Export
     styles[:grading_scale_sum_inner] = workbook.add_format grading_scale_footer_cell_attrs
     styles[:grading_scale_sum_inner_last] = workbook.add_format grading_scale_footer_cell_attrs.merge(right: BORDER_THICKNESS, right_color: BLACK_COLOR)
 
-    grading_scale_footer_last_row_cell_attrs = grading_scale_footer_cell_attrs.merge({bottom: 1, bottom_color: BLACK_COLOR})
+    grading_scale_footer_last_row_cell_attrs = grading_scale_footer_cell_attrs.merge({bottom: BORDER_THICKNESS, bottom_color: BLACK_COLOR})
     styles[:grading_scale_footer_title] = workbook.add_format grading_scale_footer_last_row_cell_attrs.merge({left: BORDER_THICKNESS, left_color: BLACK_COLOR})
     styles[:grading_scale_footer_inner] = workbook.add_format grading_scale_footer_last_row_cell_attrs
     styles[:grading_scale_footer_inner_last] = workbook.add_format grading_scale_footer_last_row_cell_attrs.merge({right: BORDER_THICKNESS, right_color: BLACK_COLOR})
@@ -154,7 +154,6 @@ class ExcelSpreadsheetExport < Export
     grading_scale_inner = {bg_color: SILVER_COLOR, align: "right"}
     styles[:grading_scale_inner] = workbook.add_format grading_scale_inner
     styles[:grading_scale_inner_last] = workbook.add_format grading_scale_inner.merge({right: BORDER_THICKNESS, left: BLACK_COLOR})
-
 
     styles[:exercise_title_cell] = workbook.add_format bold: 1, font_size: 20,
       align: "center", valign: "vcenter",
@@ -183,6 +182,19 @@ class ExcelSpreadsheetExport < Export
     grading_scale = GradingScaleService.new(term, term_registrations)
 
     worksheet = workbook.add_worksheet("summary")
+
+    # set column widths
+    worksheet.set_column 0, 0, 20
+    worksheet.set_column 1, students.count, 4
+
+    # set row heights
+    worksheet.set_row 0, 50
+    worksheet.set_row 1, 50
+    worksheet.set_row 2, 100
+    worksheet.set_row 3, 100
+    worksheet.set_row 4, 100
+    worksheet.set_row 5, 100
+
     reset_row
 
     worksheet.write same_row, 0, "#{term.course.title} #{term.title} - #{tutorial_group_title tutorial_group}", styles[:title_row]
@@ -221,39 +233,29 @@ class ExcelSpreadsheetExport < Export
     worksheet.write_row same_row, 1, term_registrations.map {|tr| grading_scale.grade_for_term_registration(tr)} , styles[:grade_cell]
 
     next_row
-    worksheet.write next_row, 1, "Grade", styles[:grading_scale_title_first]
-    worksheet.write same_row, 2, "Points", styles[:grading_scale_title]
-    worksheet.write same_row, 3, "Students", styles[:grading_scale_title]
-    worksheet.write same_row, 4, "Percent", styles[:grading_scale_title_last]
+    worksheet.merge_range next_row, 1, same_row, 2, "Grade", styles[:grading_scale_title_first]
+    worksheet.merge_range same_row, 3, same_row, 4, "Points", styles[:grading_scale_title]
+    worksheet.merge_range same_row, 5, same_row, 6, "Students", styles[:grading_scale_title]
+    worksheet.merge_range same_row, 7, same_row, 8, "Percent", styles[:grading_scale_title_last]
 
     grading_scale.grading_ranges.each do |grading_range|
-      worksheet.write next_row, 1, grading_range.grade, styles[:grading_scale_grade_title]
-      worksheet.write same_row, 2, "#{grading_range.minimum_points} - #{grading_range.maximum_ui_points}", styles[:grading_scale_inner]
-      worksheet.write same_row, 3, grading_range.student_count, styles[:grading_scale_inner]
-      worksheet.write same_row, 4, number_to_percentage(grading_scale.percent_for(grading_range.grade), precision: 1), styles[:grading_scale_inner_last]
+      worksheet.merge_range next_row, 1, same_row, 2, grading_range.grade, styles[:grading_scale_grade_title]
+      worksheet.merge_range same_row, 3, same_row, 4, "#{grading_range.minimum_points} - #{grading_range.maximum_ui_points}", styles[:grading_scale_inner]
+      worksheet.merge_range same_row, 5, same_row, 6, grading_range.student_count, styles[:grading_scale_inner]
+      worksheet.merge_range same_row, 7, same_row, 8, number_to_percentage(grading_scale.percent_for(grading_range.grade), precision: 1), styles[:grading_scale_inner_last]
     end
 
-    worksheet.write next_row, 1, "Sum", styles[:grading_scale_sum_title]
-    worksheet.write same_row, 2, "", styles[:grading_scale_sum_inner]
-    worksheet.write same_row, 3, grading_scale.graded_count, styles[:grading_scale_sum_inner]
-    worksheet.write same_row, 4, '', styles[:grading_scale_sum_inner_last]
+    worksheet.merge_range next_row, 1, same_row, 2, "Sum", styles[:grading_scale_sum_title]
+    worksheet.merge_range same_row, 3, same_row, 4, "", styles[:grading_scale_sum_inner]
+    worksheet.merge_range same_row, 5, same_row, 6, grading_scale.graded_count, styles[:grading_scale_sum_inner]
+    worksheet.merge_range same_row, 7, same_row, 8, '', styles[:grading_scale_sum_inner_last]
 
-    worksheet.write next_row, 1, "Ungraded", styles[:grading_scale_footer_title]
-    worksheet.write same_row, 2, "", styles[:grading_scale_footer_inner]
-    worksheet.write same_row, 3, grading_scale.ungraded_count, styles[:grading_scale_footer_inner]
-    worksheet.write same_row, 4, '', styles[:grading_scale_footer_inner_last]
+    worksheet.merge_range next_row, 1, same_row, 2, "Ungraded", styles[:grading_scale_footer_title]
+    worksheet.merge_range same_row, 3, same_row, 4, "", styles[:grading_scale_footer_inner]
+    worksheet.merge_range same_row, 5, same_row, 6, grading_scale.ungraded_count, styles[:grading_scale_footer_inner]
+    worksheet.merge_range same_row, 7, same_row, 8, '', styles[:grading_scale_footer_inner_last]
 
-    # set column widths
-    worksheet.set_column 0, 0, 20
-    worksheet.set_column 1, students.count, 4
 
-    # set row heights
-    worksheet.set_row 0, 50
-    worksheet.set_row 1, 50
-    worksheet.set_row 2, 100
-    worksheet.set_row 3, 100
-    worksheet.set_row 4, 100
-    worksheet.set_row 5, 100
 
     worksheet
   end

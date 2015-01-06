34cec71cabda98e82e45bcdce613abd1e0bd6fe7
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2015-01-06 12:09:36 +0000

    fix CSV parsing error on Ruby 2.2

--- lib/import/parser.rb
+++ lib/import/parser.rb
@@ -11,11 +11,7 @@ module Import::Parser
     @headers = []
     @values = []
 
-    text = load_csv_text
-
-    text.split(/\n/).each_with_index do |line, index|
-      line.strip!
-
+    load_csv_text.each_with_index do |line, index|
       values = parse_csv_line(line, csv_options)
 
       if index == 0 && import_options.headers_on_first_line
@@ -34,15 +30,16 @@ module Import::Parser
     begin
       text = File.open(import.file.to_s, 'r').read
       text = text.force_encoding("UTF-8").gsub("\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+      text.split(/\n/)
     rescue Exception => e
       import_result.update! encoding_error: true
-      text = "" # empty string should not harm anybody else
+      text = [] # no lines
     end
   end
 
   def parse_csv_line(line, csv_options)
     begin
-      values = CSV.parse_line(line, csv_options).keep_if { |cell| cell.present? }
+      values = CSV.parse_line(line.strip, csv_options).keep_if { |cell| cell.present? }
     rescue CSV::MalformedCSVError => e
       import_result.update! parsing_error: true
       values = [line]

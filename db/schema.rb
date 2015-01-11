b189c0ec552ae9410987b2246467625d20e9cac6
Thomas Kriechbaumer <thomas.kriechbaumer@student.tugraz.at>
2015-01-11 21:23:26 +0000

    fix migration timestamp
    it would not be run otherwise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111112731) do
+ActiveRecord::Schema.define(version: 20150111211840) do
 
   create_table "accounts", force: true do |t|
     t.string   "email",                  default: "",    null: false

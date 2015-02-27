58c02cef86f654180fd88ddaaaeca9ea9767df72
Matthias Link <matthias@link.co.at>
2015-02-27 22:44:21 +0100

    added submissions to student_groups and validating title presence

--- app/models/student_group.rb
+++ app/models/student_group.rb
@@ -4,13 +4,13 @@ class StudentGroup < ActiveRecord::Base
   has_one :term, through: :tutorial_group
   has_many :term_registrations
   has_many :students, through: :term_registrations, class_name: "Account", source: :account
+  has_many :submissions, dependent: :nullify
 
   scope :for_term, lambda { |term| joins{tutorial_group.term}.where{tutorial_group.term.id == my{term.id}} }
   scope :for_tutorial_group, lambda { |tutorial_group| where(tutorial_group_id: tutorial_group.id) }
 
-
+  validates :title, presence: true
   validates :tutorial_group, presence: true
-
 
   def update_points!
     self.points = self.submission_evaluations.pluck(:evaluation_result).sum

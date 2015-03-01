2c16de0f5cfee2282f7f752c70c5a7e6ea911238
Matthias Link <matthias@link.co.at>
2015-03-01 19:39:47 +0100

    do not remove non existent foreign key

--- db/migrate/20150301175541_remove_student_group_registration_from_submissions.rb
+++ db/migrate/20150301175541_remove_student_group_registration_from_submissions.rb
@@ -1,6 +1,5 @@
 class RemoveStudentGroupRegistrationFromSubmissions < ActiveRecord::Migration
   def change
     remove_reference :submissions, :student_group_registration, index: true
-    remove_foreign_key :submissions, :student_group_registrations
   end
 end

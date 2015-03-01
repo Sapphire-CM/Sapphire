62481efc47a520cf6a349864fb55b1bac75893fd
Matthias Link <matthias@link.co.at>
2015-03-01 02:03:09 +0100

    creating exercise registrations for team members

--- app/services/submission_creation_service.rb
+++ app/services/submission_creation_service.rb
@@ -23,9 +23,14 @@ class SubmissionCreationService
 
   def save
     ensure_model_setup!
-    if result = @submission.save
-      create_exercise_registrations!
+
+    result = false
+    ActiveRecord::Base.transaction do
+      if result = @submission.save
+        create_exercise_registrations!
+      end
     end
+
     result
   end
 
@@ -43,7 +48,7 @@ class SubmissionCreationService
   def setup_model!
     @submission.submitter = @account
     @submission.submitted_at = Time.now
-
+    @submission.student_group = term_registration.student_group unless @submission.exercise.solitary_submission?
     @model_setup = true
   end
 
@@ -51,14 +56,18 @@ class SubmissionCreationService
     @model_setup
   end
 
-  def create_exercise_registrations!
-    if @submission.exercise.solitary_submission?
-      term_registration = @account.term_registrations.students.find_by_term_id(@submission.exercise.term_id)
-      raise ActiveRecord::RecordNotFound unless term_registration
+  def term_registration
+    @term_registration ||= @account.term_registrations.students.find_by!(term_id: @submission.exercise.term_id)
+  end
 
+  def create_exercise_registrations!
+    if @submission.exercise.solitary_submission? || term_registration.student_group.blank?
       ExerciseRegistration.create!(submission: @submission, exercise: @submission.exercise, term_registration: term_registration)
     else
-      # TODO: fetch group members and create exercise registrations for them as well
+      student_group = term_registration.student_group
+      student_group.term_registrations.each do |term_registration|
+        ExerciseRegistration.create!(submission: @submission, exercise: @submission.exercise, term_registration: term_registration)
+      end
     end
   end
 end

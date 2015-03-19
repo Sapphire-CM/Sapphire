4103c89b755971438cebae95f90af3f1c9d1bee9
Matthias Link <matthias@link.co.at>
2015-03-19 18:40:25 +0100

    added failing spec for when a tutor navigates to students#{index, show}

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe StudentsController do
   let(:term) { FactoryGirl.create :term }
 
   describe 'GET index' do
-    it 'lists all registered students' do
-      FactoryGirl.create_list :tutorial_group, 4, term: term
-      students = FactoryGirl.create_list :term_registration, 4, :student, term: term
-      tutors = FactoryGirl.create_list :term_registration, 4, :tutor, term: term
-      lecturers = FactoryGirl.create_list :term_registration, 4, :lecturer, term: term
+    let!(:students) { FactoryGirl.create_list :term_registration, 4, :student, term: term }
+    let!(:tutors) { FactoryGirl.create_list :term_registration, 4, :tutor, term: term }
+    let!(:lecturers) { FactoryGirl.create_list :term_registration, 4, :lecturer, term: term }
 
+    it 'lists all registered students' do
       get :index, term_id: term.id
 
       expect(response).to have_http_status(:success)
@@ -22,12 +21,29 @@ RSpec.describe StudentsController do
       expect(assigns(:term_registrations)).not_to include(*lecturers)
       expect(assigns(:grading_scale)).to be_a(GradingScaleService)
     end
+
+    context 'as a tutor' do
+      let(:tutorial_group) { FactoryGirl.create(:tutorial_group, term: term) }
+      let!(:tutor_registration) { FactoryGirl.create(:term_registration, :tutor, term: term, tutorial_group: tutorial_group) }
+
+      before :each do
+        account = tutor_registration.account
+        sign_in account
+      end
+
+      it 'returns a successful response' do
+        get :index, term_id: term.id
+
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 
   describe 'GET show' do
     let(:tutorial_group) { create(:tutorial_group, term: term) }
     let(:term_registration) {create(:term_registration, :student, term: term, tutorial_group: tutorial_group) }
     let(:student_group) { create(:student_group, tutorial_group: tutorial_group) }
+
     it 'assigns the requested exercise_registrations as @exercise_registrations' do
       term = FactoryGirl.create :term
       term_registration = FactoryGirl.create :term_registration, term: term
@@ -53,6 +69,22 @@ RSpec.describe StudentsController do
 
       expect(response).to have_http_status(:success)
       expect(assigns(:student_group)).to eq(student_group)
+    end
+
+    context 'as a tutor' do
+      let(:tutorial_group) { FactoryGirl.create(:tutorial_group, term: term) }
+      let!(:tutor_registration) { FactoryGirl.create(:term_registration, :tutor, term: term, tutorial_group: tutorial_group) }
+
+      before :each do
+        account = tutor_registration.account
+        sign_in account
+      end
+
+      it 'returns a successful response' do
+        get :show, term_id: term.id, id: term_registration.id
+
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end

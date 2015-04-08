ade156562a81a64641d7ead7300925e94bbcdea5
Matthias Link <matthias@link.co.at>
2015-04-08 12:27:04 +0200

    Added StaffPolicy specs

--- spec/policies/staff_policy_spec.rb
+++ spec/policies/staff_policy_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe StaffPolicy do
-  it 'needs to be implemented'
+  let(:term) { FactoryGirl.create(:term) }
+  subject { StaffPolicy.new(account, term) }
+
+  context 'as an admin' do
+    let(:account) { FactoryGirl.create(:account, :admin) }
+
+    it { is_expected.to permit_authorization(:index) }
+    it { is_expected.to permit_authorization(:search) }
+  end
+
+  context 'as a lecturer' do
+    let(:account) { FactoryGirl.create(:account) }
+
+    describe 'lectured term' do
+      let!(:term_registration) { FactoryGirl.create(:term_registration, :lecturer, account: account, term: term) }
+
+      it { is_expected.to permit_authorization(:index) }
+      it { is_expected.to permit_authorization(:search) }
+    end
+
+    describe 'another term' do
+      let!(:term_registration) { FactoryGirl.create(:term_registration, :lecturer, account: account) }
+
+      it { is_expected.not_to permit_authorization(:index) }
+      it { is_expected.not_to permit_authorization(:search) }
+    end
+  end
+
+  context 'as a tutor' do
+    let(:account) { FactoryGirl.create(:account) }
+
+    describe 'tutored term' do
+      let!(:term_registration) { FactoryGirl.create(:term_registration, :tutor, account: account, term: term) }
+
+      it { is_expected.to permit_authorization(:index) }
+      it { is_expected.not_to permit_authorization(:search) }
+    end
+
+    describe 'another term' do
+      let!(:term_registration) { FactoryGirl.create(:term_registration, :tutor, account: account) }
+
+      it { is_expected.not_to permit_authorization(:index) }
+      it { is_expected.not_to permit_authorization(:search) }
+    end
+  end
+
+  context 'as a student' do
+    let(:account) { FactoryGirl.create(:account) }
+
+    describe 'attended term' do
+      let!(:term_registration) { FactoryGirl.create(:term_registration, :student, account: account, term: term) }
+
+      it { is_expected.not_to permit_authorization(:index) }
+      it { is_expected.not_to permit_authorization(:search) }
+    end
+
+    describe 'another term' do
+      let!(:term_registration) { FactoryGirl.create(:term_registration, :student, account: account) }
+
+      it { is_expected.not_to permit_authorization(:index) }
+      it { is_expected.not_to permit_authorization(:search) }
+    end
+  end
 end

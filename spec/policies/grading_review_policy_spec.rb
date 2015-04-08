201aa825a2b08d793a8722c974024f6955a008e4
Matthias Link <matthias@link.co.at>
2015-04-08 10:56:33 +0200

    Added GradingReviewPolicy specs

--- spec/policies/grading_review_policy_spec.rb
+++ spec/policies/grading_review_policy_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe GradingReviewPolicy do
-  it 'needs to be implemented'
+  subject { GradingReviewPolicy.new(account, term) }
+
+  let(:term) { FactoryGirl.create(:term) }
+
+  context 'as an admin' do
+    let(:account) { FactoryGirl.create(:account, :admin) }
+
+    it { is_expected.to permit_authorization(:index) }
+    it { is_expected.to permit_authorization(:show) }
+  end
+
+  %I(lecturer tutor).each do |role|
+    context "as a #{role}" do
+      let(:account) { FactoryGirl.create(:account) }
+      let!(:term_registration) { FactoryGirl.create(:term_registration, role, term: term, account: account) }
+
+      it { is_expected.to permit_authorization(:index) }
+      it { is_expected.to permit_authorization(:show) }
+    end
+  end
+
+  context 'as a student' do
+    let(:account) { FactoryGirl.create(:account) }
+    let!(:term_registration) { FactoryGirl.create(:term_registration, :student, term: term, account: account) }
+
+    it { is_expected.not_to permit_authorization(:index) }
+    it { is_expected.not_to permit_authorization(:show) }
+  end
 end

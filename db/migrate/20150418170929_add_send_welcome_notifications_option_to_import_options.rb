c1ea92f030ae9cc221740b349a5cfe5500601c8f
Matthias Link <matthias@link.co.at>
2015-04-20 13:12:25 +0200

    fixed default value for latest migration on mysql

--- db/migrate/20150418170929_add_send_welcome_notifications_option_to_import_options.rb
+++ db/migrate/20150418170929_add_send_welcome_notifications_option_to_import_options.rb
@@ -1,5 +1,5 @@
 class AddSendWelcomeNotificationsOptionToImportOptions < ActiveRecord::Migration
   def change
-    add_column :import_options, :send_welcome_notifications, :boolean, default: :true
+    add_column :import_options, :send_welcome_notifications, :boolean, default: true
   end
 end

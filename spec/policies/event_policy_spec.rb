c79c9b33bea7310c4eb475a22c4fd08e3ee25ea1
Matthias Link <matthias@link.co.at>
2015-05-20 17:26:49 +0200

    fixed test suite

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -1,28 +1,10 @@
 require 'spec_helper'
 
-describe EventPolicy do
+RSpec.describe EventPolicy do
 
   let(:user) { User.new }
 
   subject { EventPolicy }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  it 'needs to be implemented'
 end

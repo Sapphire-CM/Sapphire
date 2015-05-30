6c6a68ac3d399682b68964f78d1523217252f638
Matthias Link <matthias@link.co.at>
2015-05-30 14:00:56 +0200

    refactored event factory

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -1,15 +1,20 @@
 FactoryGirl.define do
-  factory :submission_created_event, class: Events::Submission::Created do
-    subject { create(:submission) }
+  factory :event do
     account
-    term { subject.exercise.term }
-    data {
-      {
-        submission_id: subject.id,
-        exercise_id: subject.exercise.id,
-        exercise_title: subject.exercise.title,
-        files: subject.submission_assets.map {|sa| File.join(sa.path, File.basename(sa.file.to_s)) }
+    term
+    subject { create(:term) }
+
+    factory :submission_created_event, class: Events::Submission::Created do
+      subject { create(:submission) }
+      term { subject.exercise.term }
+      data {
+        {
+          submission_id: subject.id,
+          exercise_id: subject.exercise.id,
+          exercise_title: subject.exercise.title,
+          files: subject.submission_assets.map {|sa| File.join(sa.path, File.basename(sa.file.to_s)) }
+        }
       }
-    }
+    end
   end
 end

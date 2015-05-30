27d8dc3886afd50e0618eb07aa64d711d8fb89fb
Matthias Link <matthias@link.co.at>
2015-05-30 14:09:54 +0200

    added event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,4 +8,49 @@ RSpec.describe Event, :type => :model do
   end
 
   it_behaves_like 'hash serialization', %I(data)
+
+  context 'scoping' do
+    describe '#for_term' do
+      let(:term) { FactoryGirl.create(:term) }
+      let(:another_term) { FactoryGirl.create(:term) }
+
+      let!(:events_of_term) { FactoryGirl.create_list(:event, 3, term: term) }
+      let!(:events_of_other_term) { FactoryGirl.create_list(:event, 3, term: another_term) }
+
+      it 'returns events only related to given term' do
+        expect(Event.for_term(term)).to match_array(events_of_term)
+      end
+    end
+
+    describe '#time_ordered' do
+      let(:term) { create(:term) }
+
+      it 'returns events ordered by descending creation date' do
+        event_1 = FactoryGirl.create(:event, term: term, created_at: 5.days.ago)
+        event_2 = FactoryGirl.create(:event, term: term, created_at: 3.days.ago)
+        event_3 = FactoryGirl.create(:event, term: term, created_at: 7.days.ago)
+
+        expect(Event.time_ordered).to eq([event_2, event_1, event_3])
+      end
+    end
+  end
+
+  describe '.data_reader' do
+    class DummyEvent < Event
+      data_reader :foo, :bar
+    end
+
+    subject { DummyEvent.new }
+
+    let(:dummy_data) { double }
+
+    it 'defines instance methods which access data[attribute]' do
+      expect(subject).to receive(:data).and_return(dummy_data).twice
+      expect(dummy_data).to receive(:[]).with(:foo).once.and_return('foo')
+      expect(dummy_data).to receive(:[]).with(:bar).once.and_return('bar')
+
+      expect(subject.foo).to eq('foo')
+      expect(subject.bar).to eq('bar')
+    end
+  end
 end

d22920b3270c5b79b03ab7ef19d1fa7b23b9ea13
Matthias Link <matthias@link.co.at>
2015-06-01 22:34:09 +0200

    implemented Events::RatingGroup::Updated#tracked_changes spec

--- spec/models/events/rating_group/updated_spec.rb
+++ spec/models/events/rating_group/updated_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe Events::RatingGroup::Updated do
   it { is_expected.to have_data_reader(:exercise_id) }
   it { is_expected.to have_data_reader(:rating_group_title) }
   it { is_expected.to have_data_reader(:rating_group_id) }
+
+  describe '#tracked_changes' do
+    it 'returns a hash with indifferent access' do
+      subject.data = { changes: { 'foo' => 4242 } }
+      expect(subject.tracked_changes[:foo]).to eq(4242)
+    end
+  end
 end

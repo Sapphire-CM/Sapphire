ad78258f193f8f9f442578953f06dd603063b8ec
Matthias Link <matthias@link.co.at>
2015-07-26 22:55:45 +0200

    added account management feature spec

--- spec/support/setup/feature_helpers.rb
+++ spec/support/setup/feature_helpers.rb
@@ -8,10 +8,14 @@ module FeatureHelpers
   def sign_in(account = FactoryGirl.create(:account))
     ensure_logged_out!
 
+    sign_in_with(account.email, account.password)
+  end
+
+  def sign_in_with(email, password)
     visit new_account_session_path
 
-    fill_in 'Email', with: account.email
-    fill_in 'Password', with: account.password
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
 
     click_on 'Sign in'
 
@@ -21,6 +25,12 @@ module FeatureHelpers
   def sign_out
     visit destroy_account_session_path
     @current_account = nil
+  end
+
+  def click_top_bar_link(*args)
+    within '.top-bar' do
+      click_link *args
+    end
   end
 end
 

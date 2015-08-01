91fd264d97421f6abb795e34c43ccdfa0a14b480
Matthias Link <matthias@link.co.at>
2015-08-01 10:38:49 +0200

    Add exercise feature specs

--- spec/features/course_management/exercises_spec.rb
+++ spec/features/course_management/exercises_spec.rb
@@ -1,6 +1,62 @@
 require 'rails_helper'
 
 RSpec.feature 'Exercise Management' do
-  scenario 'navigating to exercise administration page'
-  scenario 'updating exercise'
+  let(:account) { FactoryGirl.create(:account, :admin) }
+  let(:course) { FactoryGirl.create(:course) }
+  let(:term) { FactoryGirl.create(:term, course: course) }
+  let!(:exercise) { FactoryGirl.create(:exercise, term: term) }
+
+  before(:each) do
+    sign_in account
+  end
+
+  scenario 'navigating to exercise administration page' do
+    visit root_path
+
+    click_top_bar_link exercise.term.title
+    click_top_bar_link exercise.title
+
+    click_side_nav_link 'Administrate'
+    expect(page.current_path).to eq(edit_exercise_path(exercise))
+  end
+
+  scenario 'creating an exercise' do
+    visit term_path(term)
+
+    click_side_nav_link('Exercises')
+    click_link 'Add Exercise'
+
+    expect(page.current_path).to eq(new_term_exercise_path(term))
+
+    fill_in 'Title', with: 'My shiny new Exercise'
+
+    expect do
+      click_button 'Save'
+    end.to change(Exercise, :count).by(1)
+
+    expect(Exercise.last.title).to eq('My shiny new Exercise')
+  end
+
+  scenario 'updating exercise' do
+    visit edit_exercise_path(exercise)
+
+    expect(exercise.title).not_to eq('My fancy Exercise')
+
+    fill_in 'Title', with: 'My fancy Exercise'
+    click_button 'Save'
+
+    exercise.reload
+
+    expect(exercise.title).to eq('My fancy Exercise')
+  end
+
+  scenario 'removing an exercise' do
+    visit edit_exercise_path(exercise)
+
+    expect do
+      click_link 'Delete Exercise'
+    end.to change(Exercise, :count).by(-1)
+
+    expect(Exercise.find_by(id: exercise.id)).not_to be_present
+  end
 end

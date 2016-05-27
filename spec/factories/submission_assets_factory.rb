1bbc2c87b8671998b7ea60f6cdd3d3d4a94d7d20
Matthias Link <matthias@link.co.at>
2016-05-27 21:05:18 +0200

    Fix submission assets factory

--- spec/factories/submission_assets_factory.rb
+++ spec/factories/submission_assets_factory.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
     submitted_at '2015-01-01 09:10:11'
     content_type 'foobar'
     file { prepare_static_test_file 'simple_submission.txt', open: true }
-    path { |i| "simple/submission/path#{i}"}
+    sequence(:path) {|i| "simple/submission/path#{i}" }
   end
 end

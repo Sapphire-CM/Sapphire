eb99f67a975f8113d3eb114e2231bc87b504b624
Matthias Link <matthias@link.co.at>
2017-03-06 19:02:07 +0100

    Improve Sidekiq scoping

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'Sapphire'}
+  config.redis = { :namespace => "sapphire_#{Rails.env}_sidekiq"}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'Sapphire' }
+  config.redis = { :namespace => "sapphire_#{Rails.env}_sidekiq" }
 end

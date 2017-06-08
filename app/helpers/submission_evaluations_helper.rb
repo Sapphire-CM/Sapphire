b2bd1d2cbb57730796faaf4cb539deedd56f5893
Matthias Link <matthias@link.co.at>
2017-06-08 16:56:51 +0200

    Nuke single evaluations controller

--- app/helpers/submission_evaluations_helper.rb
+++ app/helpers/submission_evaluations_helper.rb
@@ -1,2 +1,5 @@
 module SubmissionEvaluationsHelper
+  def submission_evaluation_pluralized_points(submission_evaluation)
+    "#{submission_evaluation.evaluation_result} of #{pluralize submission_evaluation.submission.exercise.achievable_points, 'point'}"
+  end
 end

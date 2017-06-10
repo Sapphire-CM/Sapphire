ce2b665fe0ce68c634f3ee020225829588175574
Matthias Link <matthias@link.co.at>
2017-06-10 02:35:35 +0200

    Fix evaluation factories

--- spec/factories/evaluation_factory.rb
+++ spec/factories/evaluation_factory.rb
@@ -1,12 +1,11 @@
 FactoryGirl.define do
-  factory :variable_evaluation, class: Evaluations::VariableEvaluation do
-    evaluation_group
-    rating { variable_points_deduction_rating }
-  end
-
   factory :fixed_evaluation, class: Evaluations::VariableEvaluation do
     evaluation_group
-    rating { fixed_points_deduction_rating }
+    association :rating, factory: :fixed_points_deduction_rating
   end
 
+  factory :variable_evaluation, class: Evaluations::VariableEvaluation do
+    evaluation_group
+    association :rating, factory: :variable_points_deduction_rating
+  end
 end

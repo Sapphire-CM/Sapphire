d73a6f2b2a395617e25cc2a0b142b6e3ee5f01b3
Matthias Link <matthias@link.co.at>
2017-06-13 09:16:14 +0200

    Remove single evaluations links

--- app/controllers/submission_viewers_controller.rb
+++ app/controllers/submission_viewers_controller.rb
@@ -8,7 +8,7 @@ class SubmissionViewersController < ApplicationController
     if @viewer
       @viewer.params = params
     else
-      redirect_to single_evaluation_path(@submission), alert: 'There is no viewer for this submission'
+      redirect_to submission_evaluation_path(@submission.submission_evaluation), alert: 'There is no viewer for this submission'
     end
   end
 end

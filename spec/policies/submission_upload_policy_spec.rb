1a5860a4a1b9141abbcd216b96fe38335b5bd14b
Matthias Link <matthias@link.co.at>
2017-11-27 20:29:17 +0100

    Add submission upload policy specs

--- spec/policies/submission_upload_policy_spec.rb
+++ spec/policies/submission_upload_policy_spec.rb
@@ -1,28 +1,68 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe SubmissionUploadPolicy do
+RSpec.describe SubmissionUploadPolicy do
+  let(:submission_upload) { SubmissionUpload.new(submission: submission) }
+  subject { described_class.new(account, submission_upload) }
 
-  let(:user) { User.new }
+  context 'as admin' do
+    let(:account) { FactoryGirl.create(:account, :admin) }
+    let(:submission) { FactoryGirl.create(:submission) }
 
-  subject { SubmissionUploadPolicy }
-
-  describe ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it { is_expected.to permit_authorization :new }
+    it { is_expected.to permit_authorization :create }
   end
 
-  describe :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  %I(lecturer tutor).each do |role|
+    context "as #{role}" do
+      let(:account) { FactoryGirl.create(:account, role) }
+
+      context 'of term' do
+        let(:term_registration) { account.term_registrations.last }
+        let(:term) { term_registration.term }
+        let(:exercise) { FactoryGirl.create(:exercise, term: term) }
+        let(:submission) { FactoryGirl.create(:submission, exercise: exercise) }
+
+        it { is_expected.to permit_authorization :new }
+        it { is_expected.to permit_authorization :create }
+      end
+
+      context 'of other term' do
+        let(:submission) { FactoryGirl.create(:submission) }
+
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+    end
   end
 
-  describe :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context "as student" do
+    let(:account) { FactoryGirl.create(:account, :student) }
 
-  describe :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context 'of term' do
+      let(:term_registration) { account.term_registrations.last }
+      let(:term) { term_registration.term }
+      let(:exercise) { FactoryGirl.create(:exercise, term: term) }
+      let(:submission) { FactoryGirl.create(:submission, exercise: exercise) }
 
-  describe :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+      context 'owning the submission' do
+        let!(:exercise_registration) { FactoryGirl.create(:exercise_registration, exercise: exercise, term_registration: term_registration, submission: submission) }
+
+        it { is_expected.to permit_authorization :new }
+        it { is_expected.to permit_authorization :create }
+      end
+
+      context 'not owning the submission' do
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+
+    end
+
+    context 'of other term' do
+      let(:submission) { FactoryGirl.create(:submission) }
+
+      it { is_expected.not_to permit_authorization :new }
+      it { is_expected.not_to permit_authorization :create }
+    end
   end
 end

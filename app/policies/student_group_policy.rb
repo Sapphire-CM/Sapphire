612191cffdbfe188dc207df518d5caedc2fea533
Matthias Link <matthias@link.co.at>
2017-11-27 22:05:59 +0100

    Refactor student group policy

--- app/policies/student_group_policy.rb
+++ app/policies/student_group_policy.rb
@@ -1,35 +1,25 @@
 class StudentGroupPolicy < TermBasedPolicy
   def index?
-    user.admin? || user.staff_of_term?(record.term)
+    staff_permissions?
   end
 
   def show?
-    user.admin? || user.staff_of_term?(record.term)
+    staff_permissions?
   end
 
   def create?
-    authorized?(record.term)
+    lecturer_permissions?
   end
 
   def update?
-    authorized?
+    lecturer_permissions?
   end
 
   def destroy?
-    authorized?
+    lecturer_permissions?
   end
 
   def search_students?
-    authorized?(record.term)
-  end
-
-  private
-
-  def authorized?(r = nil)
-    user.admin? || if !r.nil?
-      user.lecturer_of_term?(r)
-    elsif record.term.present?
-      user.lecturer_of_term?(record.term)
-    end
+    lecturer_permissions?
   end
 end

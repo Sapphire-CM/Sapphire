bf9a134cc05fd2dc0c8a903cef35079a0abf79eb
Matthias Link <matthias@link.co.at>
2017-11-27 16:45:43 +0100

    Refactor term policy specs

--- spec/policies/term_policy_spec.rb
+++ spec/policies/term_policy_spec.rb
@@ -1,39 +1,238 @@
 require 'rails_helper'
 
-describe TermPolicy do
-  subject { Pundit.policy(user, term) }
+RSpec.describe TermPolicy do
+  context 'as an admin' do
+    let(:account) { FactoryGirl.create(:account, :admin) }
+    let(:term) { FactoryGirl.create(:term) }
 
-  let(:term) { FactoryGirl.create(:term) }
+    describe 'scoping' do
+      subject { described_class::Scope.new(account, Term.all) }
+      let(:terms) { FactoryGirl.create_list(:term, 2) + [term] }
 
-  context 'as a student' do
-    let(:tutorial_group) { FactoryGirl.create(:tutorial_group, term: term) }
-    let(:user) do
-      account = FactoryGirl.create(:account)
-      FactoryGirl.create(:term_registration, :student, account: account, term: term, tutorial_group: tutorial_group)
-      account
+      it 'contains all terms' do
+        expect(subject.resolve).to match_array(terms)
+      end
     end
 
-    it { is_expected.to permit_authorization :student }
-    it { is_expected.not_to permit_authorization :tutor }
+    describe 'permissions' do
+      subject { Pundit.policy(account, term) }
+
+      context 'new term' do
+        let(:term) { FactoryGirl.build(:term) }
+
+        it { is_expected.to permit_authorization :new }
+        it { is_expected.to permit_authorization :create }
+      end
+
+      context 'existing term' do
+        it { is_expected.to permit_authorization :show }
+        it { is_expected.to permit_authorization :edit }
+        it { is_expected.to permit_authorization :update }
+        it { is_expected.to permit_authorization :destroy }
+        it { is_expected.to permit_authorization :points_overview }
+        it { is_expected.to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.not_to permit_authorization :staff }
+      end
+    end
   end
 
-  context 'as an admin' do
-    let(:tutorial_group) { FactoryGirl.create(:tutorial_group, term: term) }
-    let(:user) { FactoryGirl.create(:account, :admin) }
+  context 'as a student' do
+    let(:account) { FactoryGirl.create(:account, :student) }
+    let(:term_registration) { account.term_registrations.student.first }
 
-    it { is_expected.not_to permit_authorization :student }
-    it { is_expected.not_to permit_authorization :tutor }
+    describe 'scoping' do
+      subject { described_class::Scope.new(account, Term.all) }
+      let(:student_term) { term_registration.term }
+      let(:sibling_term) { FactoryGirl.create(:term, course: student_term.course) }
+      let(:other_term) { FactoryGirl.create(:term) }
+      let!(:terms) { [student_term, sibling_term, other_term] }
+
+      it 'contains only participated terms' do
+        expect(subject.resolve).to match_array([student_term])
+      end
+    end
+
+    describe 'permissions' do
+      subject { Pundit.policy(account, term) }
+
+      context 'new term' do
+        let(:term) { FactoryGirl.build(:term) }
+
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+
+      context 'new sibling term' do
+        let(:student_term) { term_registration.term }
+        let(:term) { FactoryGirl.build(:term, course: student_term.course) }
+
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+
+      context 'student of term' do
+        let(:term) { term_registration.term }
+
+        it { is_expected.to permit_authorization :show }
+        it { is_expected.not_to permit_authorization :edit }
+        it { is_expected.not_to permit_authorization :update }
+        it { is_expected.not_to permit_authorization :destroy }
+        it { is_expected.not_to permit_authorization :points_overview }
+        it { is_expected.not_to permit_authorization :send_welcome_notifications }
+        it { is_expected.to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.not_to permit_authorization :staff }
+      end
+
+      context 'student of other term' do
+        let(:term) { FactoryGirl.create(:term) }
+
+        it { is_expected.not_to permit_authorization :show }
+        it { is_expected.not_to permit_authorization :edit }
+        it { is_expected.not_to permit_authorization :update }
+        it { is_expected.not_to permit_authorization :destroy }
+        it { is_expected.not_to permit_authorization :points_overview }
+        it { is_expected.not_to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.not_to permit_authorization :staff }
+      end
+    end
   end
 
   context 'as a tutor' do
-    let(:user) do
-      account = FactoryGirl.create(:account, :tutor)
-      FactoryGirl.create(:term_registration, :tutor, account: account, term: term, tutorial_group: tutorial_group)
-      account
-    end
-    let(:tutorial_group) { FactoryGirl.create(:tutorial_group, term: term) }
+    let(:account) { FactoryGirl.create(:account, :tutor) }
+    let(:term_registration) { account.term_registrations.tutor.first }
 
-    it { is_expected.not_to permit_authorization :student }
-    it { is_expected.to permit_authorization :tutor }
+    describe 'scoping' do
+      subject { described_class::Scope.new(account, Term.all) }
+
+      let(:tutor_term) { term_registration.term }
+      let(:sibling_term) { FactoryGirl.create(:term, course: tutor_term.course) }
+      let(:other_term) { FactoryGirl.create(:term) }
+      let!(:terms) { [tutor_term, sibling_term, other_term] }
+
+      it 'contains only tutored terms' do
+        expect(subject.resolve).to match_array([tutor_term])
+      end
+    end
+
+    describe 'permissions' do
+      subject { Pundit.policy(account, term) }
+
+      context 'new term' do
+        let(:term) { FactoryGirl.build(:term) }
+
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+
+      context 'new sibling term' do
+        let(:tutored_term) { term_registration.term }
+        let(:term) { FactoryGirl.build(:term, course: tutored_term.course) }
+
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+
+      context 'of term' do
+        let(:term) { term_registration.term }
+
+        it { is_expected.to permit_authorization :show }
+        it { is_expected.not_to permit_authorization :edit }
+        it { is_expected.not_to permit_authorization :update }
+        it { is_expected.not_to permit_authorization :destroy }
+        it { is_expected.to permit_authorization :points_overview }
+        it { is_expected.not_to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.to permit_authorization :tutor }
+        it { is_expected.to permit_authorization :staff }
+      end
+
+      context 'of other term' do
+        let(:term) { FactoryGirl.create(:term) }
+
+        it { is_expected.not_to permit_authorization :show }
+        it { is_expected.not_to permit_authorization :edit }
+        it { is_expected.not_to permit_authorization :update }
+        it { is_expected.not_to permit_authorization :destroy }
+        it { is_expected.not_to permit_authorization :points_overview }
+        it { is_expected.not_to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.not_to permit_authorization :staff }
+      end
+    end
+  end
+
+
+  context 'as a lecturer' do
+    let(:account) { FactoryGirl.create(:account, :lecturer) }
+    let(:term_registration) { account.term_registrations.lecturer.first }
+
+    describe 'permissions' do
+      subject { Pundit.policy(account, term) }
+
+      context 'new term' do
+        let(:term) { FactoryGirl.build(:term) }
+
+        it { is_expected.not_to permit_authorization :new }
+        it { is_expected.not_to permit_authorization :create }
+      end
+
+      context 'new sibling term' do
+        let(:lectured_term) { term_registration.term }
+        let(:term) { FactoryGirl.build(:term, course: lectured_term.course) }
+
+        it { is_expected.to permit_authorization :new }
+        it { is_expected.to permit_authorization :create }
+      end
+
+      context 'of term' do
+        let(:term) { term_registration.term }
+
+        it { is_expected.to permit_authorization :show }
+        it { is_expected.to permit_authorization :edit }
+        it { is_expected.to permit_authorization :update }
+        it { is_expected.to permit_authorization :destroy }
+        it { is_expected.to permit_authorization :points_overview }
+        it { is_expected.to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.to permit_authorization :staff }
+      end
+
+      context 'of other term of lectured course' do
+        let(:lectured_term) { term_registration.term }
+        let(:course) { lectured_term.course }
+        let(:term) { FactoryGirl.create(:term, course: course) }
+
+        it { is_expected.not_to permit_authorization :show }
+        it { is_expected.not_to permit_authorization :edit }
+        it { is_expected.not_to permit_authorization :update }
+        it { is_expected.not_to permit_authorization :destroy }
+        it { is_expected.not_to permit_authorization :points_overview }
+        it { is_expected.not_to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.not_to permit_authorization :staff }
+      end
+
+      context 'of other term' do
+        let(:term) { FactoryGirl.create(:term) }
+
+        it { is_expected.not_to permit_authorization :show }
+        it { is_expected.not_to permit_authorization :edit }
+        it { is_expected.not_to permit_authorization :update }
+        it { is_expected.not_to permit_authorization :destroy }
+        it { is_expected.not_to permit_authorization :points_overview }
+        it { is_expected.not_to permit_authorization :send_welcome_notifications }
+        it { is_expected.not_to permit_authorization :student }
+        it { is_expected.not_to permit_authorization :tutor }
+        it { is_expected.not_to permit_authorization :staff }
+      end
+    end
   end
 end

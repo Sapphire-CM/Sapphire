da8b2831e9b024d4aead93d1415d29e9f9559018
Matthias Link <matthias@link.co.at>
2018-02-12 17:01:52 +0100

    Redirect back after cancelling an impersonation

--- app/controllers/impersonations_controller.rb
+++ app/controllers/impersonations_controller.rb
@@ -13,9 +13,9 @@ class ImpersonationsController < ApplicationController
   def destroy
     old_account = current_account
     if @impersonation.destroy
-      redirect_to root_path, notice: "No longer impersonating '#{old_account.email}'."
+      redirect_to :back, notice: "No longer impersonating '#{old_account.email}'."
     else
-      redirect_to root_path
+      redirect_to :back
     end
   end
 

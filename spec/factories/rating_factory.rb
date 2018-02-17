8d72ea06f92067fada25ad53871778416d97d856
Matthias Link <matthias@link.co.at>
2018-02-17 13:59:33 +0100

    Refactor rating factory

--- spec/factories/rating_factory.rb
+++ spec/factories/rating_factory.rb
@@ -1,90 +1,64 @@
 FactoryGirl.define do
   sequence(:rating_title) { |n| "Rating #{n}" }
 
-  factory :fixed_points_deduction_rating, class: Ratings::FixedPointsDeductionRating do
+  factory :rating, class: Ratings::FixedPointsDeductionRating do
     title { generate :rating_title }
     rating_group
     description { generate :lorem_ipsum }
-    multiplication_factor 1.0
+    bulk false
 
+    multiplication_factor 1.0
     value -3
+
+    trait :bulk do
+      bulk true
+    end
+
+    factory :fixed_points_deduction_rating, class: Ratings::FixedPointsDeductionRating do
+      value -3
+    end
+
+    factory :fixed_percentage_deduction_rating, class: Ratings::FixedPercentageDeductionRating do
+      value -25
+    end
+
+    factory :fixed_bonus_points_rating, class: Ratings::FixedBonusPointsRating do
+      value 4
+    end
+
+    factory :variable_points_deduction_rating, class: Ratings::VariablePointsDeductionRating do
+      max_value 0
+      min_value -10
+    end
+
+    factory :variable_bonus_points_rating, class: Ratings::VariableBonusPointsRating do
+      max_value 10
+      min_value 0
+    end
+
+    factory :variable_percentage_deduction_rating, class: Ratings::VariablePercentageDeductionRating do
+      max_value 0
+      min_value -25
+    end
+
+    factory :per_item_points_deduction_rating, class: Ratings::PerItemPointsDeductionRating do
+      max_value 8
+      min_value 0
+
+      multiplication_factor -4
+    end
+
+
+    factory :per_item_points_rating, class: Ratings::PerItemPointsRating do
+      max_value 8
+      min_value 0
+
+      multiplication_factor 2
+    end
+
+    factory :plagiarism_rating, class: Ratings::PlagiarismRating do
+    end
+
   end
 
-  factory :fixed_percentage_deduction_rating, class: Ratings::FixedPercentageDeductionRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-    multiplication_factor 1.0
-
-    value -25
-  end
-
-  factory :fixed_bonus_points_rating, class: Ratings::FixedBonusPointsRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-    multiplication_factor 1.0
-
-    value 4
-  end
-
-  factory :variable_points_deduction_rating, class: Ratings::VariablePointsDeductionRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-    multiplication_factor 1.0
-
-    max_value 0
-    min_value -10
-  end
-
-  factory :variable_bonus_points_rating, class: Ratings::VariableBonusPointsRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-    multiplication_factor 1.0
-
-    max_value 10
-    min_value 0
-  end
-
-  factory :variable_percentage_deduction_rating, class: Ratings::VariablePercentageDeductionRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-    multiplication_factor 1.0
-
-    max_value 0
-    min_value -25
-  end
-
-  factory :per_item_points_deduction_rating, class: Ratings::PerItemPointsDeductionRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-
-    max_value 8
-    min_value 0
-
-    multiplication_factor -4
-  end
-
-
-  factory :per_item_points_rating, class: Ratings::PerItemPointsRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-
-    max_value 8
-    min_value 0
-
-    multiplication_factor 2
-  end
-
-  factory :plagiarism_rating, class: Ratings::PlagiarismRating do
-    title { generate :rating_title }
-    rating_group
-    description { generate :lorem_ipsum }
-    multiplication_factor 1.0
-  end
 end

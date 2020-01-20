ea51758793b8059fb110d2fc7ffb46cf661abc45
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-01-20 12:52:44 +0100

    added module controllers and comment policy

--- app/controllers/evaluations/comments_controller.rb
+++ app/controllers/evaluations/comments_controller.rb
@@ -1,16 +1,15 @@
-class SubmissionEvaluations::CommentsController < CommentsController
+class Evaluations::CommentsController < CommentsController
   before_action :set_commentable, :set_term
 
   def show
     authorize Comment
-    puts("rendering comment show")
     render "comment/show"
   end
 
   private
   
   def set_commentable
-    @commentable = SubmissionEvaluation.find(params[:submission_evaluation_id])
+    @commentable = Evaluation.find(params[:evaluation_id])
   end
 
   def set_term

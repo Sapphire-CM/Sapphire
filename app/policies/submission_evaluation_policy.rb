cdd3363fa82d08312530355472482952256aea54
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-02-07 23:41:24 +0100

    note policy and extended comments policy
    this also adds policy rules for the commentable SubmissionEvaluation,
    since pundit asks for that.

--- app/policies/submission_evaluation_policy.rb
+++ app/policies/submission_evaluation_policy.rb
@@ -3,6 +3,14 @@ class SubmissionEvaluationPolicy < TermBasedPolicy
     update?
   end
 
+  def create?
+    update? ## Comment create also asks for this permission
+  end
+
+  def destroy?
+    update? ## Comment destroy also asks for this permission
+  end
+
   def update?
     staff_permissions?(record.submission.term)
   end

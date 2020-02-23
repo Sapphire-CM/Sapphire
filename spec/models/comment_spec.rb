a1d16fe00dcb50b8d7fbdbe48245941c66ccc4d9
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-02-23 23:02:34 +0100

    added comment and according controller specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'db columns' do
+    it { is_expected.to have_db_column(:commentable_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:commentable_type).of_type(:string).with_options(null: false ) }
+    it { is_expected.to have_db_column(:account_id).of_type(:integer).with_options(null: false ) }
+    it { is_expected.to have_db_column(:term_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:content).of_type(:text).with_options(null: false)) }
+
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
+
 end

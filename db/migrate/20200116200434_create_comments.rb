71deda99fd7880740f4ace6d37fabecb57343488
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-03-03 22:34:24 +0100

    restructured backend for comments
    comments are now added to a model via `has_many_comments :type`.
    Removed notes migration since this is not needed anymore.

--- db/migrate/20200116200434_create_comments.rb
+++ db/migrate/20200116200434_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration
       t.references :commentable, index: true, null: false, polymorphic: true
       t.references :account, index: true, null: false, foreign_key: true
       t.references :term, null: false, foreign_key: true
+      t.string :name, null: false
 
       t.text :content, null: false
 

498a01bb8977b91b63fde5730b71d7acab419575
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-04-11 00:10:28 +0200

    updated and added comment specs
    This updated the model spec and adds controller specs using the newly
    added comment behaviour

---
+++ spec/controllers/submission_evaluation_feedback_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+require_relative 'behaviours/comment_behaviour'
+
+RSpec.describe SubmissionEvaluations::FeedbackController, type: :controller do
+  render_views
+  include_context 'active_admin_session_context'
+
+  let(:term) { FactoryGirl.create :term }
+  let(:exercise) { FactoryGirl.create :exercise, :with_ratings, term: term }
+
+  let(:submission) { FactoryGirl.create_list(:submission, 3, exercise: exercise)[1] }
+  let(:commentable) { submission.submission_evaluation }
+
+  describe 'GET index' do
+    it 'assigns the commentable as @commentable' do
+      xhr :get, :index, submission_evaluation_id: commentable.id
+
+      expect(response).to have_http_status(:success)
+      expect(assigns(:term)).to eq(term)
+      expect(assigns(:commentable)).to eq(commentable)
+    end
+  end
+
+  it_behaves_like 'a comment' do
+    let(:comment_with_content) do 
+      {
+        comment: {
+          content: "Some commment content",
+          name: "feedback"
+        },
+        submission_evaluation_id: commentable.id,
+        term: :term
+      }
+    end
+
+    let(:comment_without_content) do 
+      {
+        comment: {
+          content: nil
+        },
+        submission_evaluation_id: commentable.id,
+        term: :term
+      }
+    end
+
+    let(:comment) { FactoryGirl.create :feedback_comment, account: current_account, commentable: commentable }
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_db_column(:commentable_type).of_type(:string).with_options(null: false ) }
     it { is_expected.to have_db_column(:account_id).of_type(:integer).with_options(null: false ) }
     it { is_expected.to have_db_column(:term_id).of_type(:integer).with_options(null: false) }
-    it { is_expected.to have_db_column(:content).of_type(:text).with_options(null: false)) }
+    it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:content).of_type(:text).with_options(null: false) }
 
     it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }

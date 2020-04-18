34104b1fc33e4d0273ef649f859f371c9fabc78d
Amel Smajic <amel.smajic@student.tugraz.at>
2020-04-18 14:22:04 +0200

    Changed incement/decrement of submission file size to be thread safe. Made a module for disk usage statistics which should replace the statistics model. This module uses SQL methods

--- app/models/submission_asset.rb
+++ app/models/submission_asset.rb
@@ -52,7 +52,7 @@ class SubmissionAsset < ActiveRecord::Base
 
   delegate :exercise, to: :submission
 
-  after_save :add_to_submission_filsize
+  after_create :add_to_submission_filsize
   after_destroy :remove_from_submission_filesize
 
   EXCLUDED_FILTER = [
@@ -240,12 +240,10 @@ class SubmissionAsset < ActiveRecord::Base
   end
 
   def add_to_submission_filsize
-    submission.update('filesystem_size' => (filesystem_size + submission.filesystem_size)) 
-    submission.exercise.statistics.dirty_bit = true
+    submission.increment!(:filesystem_size, filesystem_size) 
   end
 
   def remove_from_submission_filesize
-    submission.update('filesystem_size' => (submission.filesystem_size - filesystem_size))
-    submission.exercise.statistics.dirty_bit = true
+    submission.decrement!(:filesystem_size, filesystem_size)
   end
 end

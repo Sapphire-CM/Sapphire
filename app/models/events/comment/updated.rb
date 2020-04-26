8d38d0150d188ad679346fd1df7b813c8a895e00
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-04-26 08:46:35 +0200

    added comments events models

---
+++ app/models/events/comment/updated.rb
@@ -0,0 +1,21 @@
+# create_table :events, force: :cascade do |t|
+#   t.string   :type
+#   t.integer  :subject_id,   null: false
+#   t.string   :subject_type, null: false
+#   t.integer  :account_id,   null: false
+#   t.integer  :term_id,      null: false
+#   t.text     :data
+#   t.datetime :created_at,   null: false
+#   t.datetime :updated_at,   null: false
+# end
+#
+# add_index :events, [:account_id], name: :index_events_on_account_id
+# add_index :events, [:subject_type, :subject_id], name: :index_events_on_subject_type_and_subject_id
+
+module Events
+  module Comment
+    class Updated < ::Event
+      data_reader :name, :commentable_type, :commentable_id, :commentable_index
+    end
+  end
+end

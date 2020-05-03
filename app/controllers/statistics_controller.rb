1f03eb2fde33fe880a2d25c4b6af44ad4279a5f4
Amel Smajic <amel.smajic@student.tugraz.at>
2020-05-03 19:20:05 +0200

    Removed statistics model and caching file size on exercise level

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,10 +3,6 @@ class StatisticsController < ApplicationController
 
   before_action :set_term, only: [:show]
 
-  def show
-        
-  end
-
   private
     def set_term
       @term = Term.find(params[:term_id])
--- db/migrate/20200329184053_add_file_size_to_exercise.rb
+++
@@ -1,5 +0,0 @@
-class AddFileSizeToExercise < ActiveRecord::Migration
-  def change
-    add_column :exercises, :filesystem_size, :integer, default: 0
-  end
-end

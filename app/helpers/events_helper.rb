2797bbc68dcdf6c259b610ff5f774baaa50e5556
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-05-09 16:27:54 +0200

    differ between student/rest for comment index link

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,4 +4,12 @@ module EventsHelper
 
     render 'events/event', content: capture(event, &block), event: event, css_classes: "event event-#{event.class.to_s.demodulize.downcase}", cache_identifier: cache_identifier
   end
+
+  def comment_event_index(event)
+    if policy(Comment.new(term: @term)).create?
+      link_to event.exercise_title, polymorphic_path(event.subject_type.underscore, id: event.subject_id)
+    else
+      link_to event.exercise_title, term_result_path(term: event.term, id: event.subject.submission.exercise.id)
+    end
+  end
 end

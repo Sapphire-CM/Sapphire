55824a6148f2547855daa41fa8081de1ae18d3d6
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-05-09 12:45:03 +0200

    added annotate color class to resemble post its

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,6 +3,8 @@ module CommentsHelper
     classes =  ["tiny", "button", "expand"]
     unless evaluation.explanations_comments.any?
       classes << ["secondary"]
+    else
+      classes << ["annotate"]
     end
     link_to "Explanation", evaluation_explanations_path(evaluation.becomes(Evaluation)), data: {remote: :true, "reveal-id" => "reveal_modal"}, class: classes.join(' '), id: dom_id(evaluation.becomes(Evaluation), 'comment_button')
   end

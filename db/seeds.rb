17202793128826c4918912f011c2208fba243bf0
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-05-26 14:17:24 +0200

    removed debug flag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 require 'date'
 
-debug = true
-
-puts "Creating Accounts..." if debug
+puts "Creating Accounts..."
 
 accounts = Account.create! [
   { email: 'tutor@testing',    forename: 'Tutor',    surname: 'Tester', password: 'testing', password_confirmation: 'testing' },
@@ -19,21 +17,21 @@ student_accounts = Account.create! [
   { email: 'student6@testing',  forename: 'Student6',  surname: 'Tester', matriculation_number: '12345676', password: 'testing', password_confirmation: 'testing' },
   { email: 'student7@testing',  forename: 'Student7',  surname: 'Tester', matriculation_number: '12345677', password: 'testing', password_confirmation: 'testing' }
 ]
-puts "Done!" if debug
+puts "Done!"
 
-puts "Creating a Course..." if debug 
+puts "Creating a Course..."
 course = Course.create!({ title: 'Test Course' })
 course.update(locked: false)
-puts "Done!" if debug
+puts "Done!"
 
-puts "Creating a Term for that Course..." if debug
+puts "Creating a Term for that Course..."
 terms = Term.create! [
   { title: "Term #{Time.now.year}",     course: course }
 ]
 Term.all.map{ |obj| obj.row_order_position = :last; obj.save }
-puts "Done!" if debug
+puts "Done!"
 
-puts "Creating Tutorial and Student Groups..." if debug
+puts "Creating Tutorial and Student Groups..."
 tutorial_groups = [
   {title: "T1", description: "First tutorial group"},
   {title: "T2", description: "Second tutorial group"}
@@ -41,9 +39,9 @@ tutorial_groups = [
 tutorial_groups.each {|tg| tg.save}
 
 student_groups = [
-  {title: "G1"}, 
-  {title: "G2"}, 
-  {title: "G3"}, 
+  {title: "G1"},
+  {title: "G2"},
+  {title: "G3"},
   {title: "G4"}
 ].map {|sg_hash| sg = StudentGroup.new(sg_hash); sg.term = course.terms.last; sg}
 
@@ -52,11 +50,11 @@ student_groups.each_slice(tutorial_groups.size).to_a.each.with_index do |groups,
   groups.each do |sg|
     sg.tutorial_group = tg
     sg.save
-  end 
+  end
 end
-puts "Done!" if debug
+puts "Done!"
 
-puts "Registering Accounts..." if debug
+puts "Registering Accounts..."
 TermRegistration.create! account: accounts[0], term: course.terms.last, role: Roles::TUTOR, tutorial_group: tutorial_groups.first
 TermRegistration.create! account: accounts[1], term: course.terms.last, role: Roles::LECTURER
 
@@ -66,17 +64,17 @@ student_accounts.each_slice(student_accounts.size / student_groups.size).to_a.ea
     TermRegistration.create! account: acc, term: course.terms.last, role: Roles::STUDENT, student_group: sg, tutorial_group: sg.tutorial_group
   end
 end
-puts "Done!" if debug
+puts "Done!"
 
-puts "Creating Exercises..." if debug
+puts "Creating Exercises..."
 exercises = Exercise.create! [
   { title: 'Ex 1: Something',  term: course.terms.last, group_submission: true },
   { title: 'MC Test',          term: course.terms.last, group_submission: false }
 ]
 Exercise.all.map{ |obj| obj.row_order_position = :last; obj.save }
-puts "Done!" if debug
+puts "Done!"
 
-puts "Creating Rating Groups and Ratings for the first Exercise..." if debug
+puts "Creating Rating Groups and Ratings for the first Exercise..."
 rating_groups = [
   {title: "Introduction",  enable_range_points: true, points: 2, max_points:2,  min_points:0, global: false},
   {title: "First Section", enable_range_points: true, points: 10, max_points:10, min_points:0, global: false},
@@ -104,11 +102,11 @@ miscellaneous_ratings = [
 ].map {|r_hash| r = Rating.new(r_hash); r.rating_group = rating_groups[2]; r}
 miscellaneous_ratings.each {|r| r.save }
 
-puts "Done!" if debug
+puts "Done!"
 
-puts "Creating a Submission for Student Group #{student_groups.first.title}" if debug
+puts "Creating a Submission for Student Group #{student_groups.first.title}"
 SubmissionCreationService.new_student_submission(student_accounts.first, exercises.first).save
-puts "Done!\n" if debug
+puts "Done!\n"
 
 puts <<-MESSAGE
 You are now good to go! You may start the server with:
@@ -121,6 +119,6 @@ Open your browser at 'localhost:3000' and login with:
  Email:'{admin,lecturer,tutor,student}@testing'
  Password:'testing'
 
-Note: The Email prefix will put you into the respective role, 
+Note: The Email prefix will put you into the respective role,
       e.g. use 'student@testing' to have a look at how students will see Sapphire.
 MESSAGE

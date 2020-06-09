09b7d1df885be76dc6d33daa46aeccb9597579e5
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-06-09 13:05:10 +0200

    adapted comment heading to fit naming of comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,4 @@
-module CommentsHelper 
+module CommentsHelper
   def evaluation_comment_button(evaluation)
     classes =  ["tiny", "button", "expand"]
     unless evaluation.explanations_comments.any?
@@ -7,5 +7,18 @@ module CommentsHelper
       classes << ["annotate"]
     end
     link_to "Explanation", evaluation_explanations_path(evaluation.becomes(Evaluation)), data: {remote: :true, "reveal-id" => "reveal_modal"}, class: classes.join(' '), id: dom_id(evaluation.becomes(Evaluation), 'comment_button')
+  end
+
+  def humanized_comment_type(comment)
+    comment.name.singularize.humanize
+  end
+
+  def comment_heading(comment)
+    case comment.name
+      when 'feedback'
+        "#{humanized_comment_type(comment)} from <strong>#{comment.account.fullname}</strong>:".html_safe
+      when 'internal_notes', 'explanations'
+        "#{humanized_comment_type(comment)} by <strong>#{comment.account.fullname}</strong>:".html_safe
+    end
   end
 end

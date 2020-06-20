d25bd0aa9701016603e60bd21c4aac2183eacffc
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-06-20 22:44:07 +0200

    submitterd id migrations now gets backfilled

--- db/migrate/20190411094443_add_submitter_id_to_submission_asset.rb
+++ db/migrate/20190411094443_add_submitter_id_to_submission_asset.rb
@@ -1,5 +1,15 @@
 class AddSubmitterIdToSubmissionAsset < ActiveRecord::Migration
-  def change
-    add_reference :submission_assets, :submitter, index: true
+  def up
+    add_reference :submission_assets, :submitter, foreign_key: { to_table: :accounts }, null: true
+
+    Submission.find_each do |submission|
+      submission.submission_assets.update_all(submitter_id: submission.submitter_id)
+    end
+
+    change_column_null :submission_assets, :submitter_id, false
+  end
+
+  def down
+    remove_reference :submission_assets, :submitter, foreign_key: true
   end
 end

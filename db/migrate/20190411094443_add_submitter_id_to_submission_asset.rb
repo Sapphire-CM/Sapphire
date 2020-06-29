e95dbd88d6a5b055994825f860955fec6fcfc637
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-06-29 13:04:41 +0200

    backfill submitter only if present
    We do already have some entries on production where we are not able to
    backfill this column

--- db/migrate/20190411094443_add_submitter_id_to_submission_asset.rb
+++ db/migrate/20190411094443_add_submitter_id_to_submission_asset.rb
@@ -3,10 +3,8 @@ class AddSubmitterIdToSubmissionAsset < ActiveRecord::Migration
     add_reference :submission_assets, :submitter, foreign_key: { to_table: :accounts }, null: true
 
     Submission.find_each do |submission|
-      submission.submission_assets.update_all(submitter_id: submission.submitter_id)
+      submission.submission_assets.update_all(submitter_id: submission.submitter_id) if submission.submitter_id.present?
     end
-
-    change_column_null :submission_assets, :submitter_id, false
   end
 
   def down

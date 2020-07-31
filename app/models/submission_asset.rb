f8aef048b68df1b7dd6877e5f17f919f99c15bde
Amel Smajic <amel.smajic@student.tugraz.at>
2020-07-31 17:24:46 +0200

    Fixed failing unit tests.

--- app/models/submission_asset.rb
+++ app/models/submission_asset.rb
@@ -52,7 +52,7 @@ class SubmissionAsset < ActiveRecord::Base
 
   delegate :exercise, to: :submission
 
-  after_create :add_to_submission_filsize
+  after_create :add_to_submission_filesize
   after_destroy :remove_from_submission_filesize
 
   EXCLUDED_FILTER = [
@@ -188,12 +188,14 @@ class SubmissionAsset < ActiveRecord::Base
     Mime::ARCHIVES.include? self.content_type
   end
 
-  def add_to_submission_filsize
-    submission.increment!(:filesystem_size, filesystem_size) 
+  def add_to_submission_filesize
+    submission.increment(:filesystem_size, filesystem_size)
+    submission.save!
   end
 
   def remove_from_submission_filesize
-    submission.decrement!(:filesystem_size, filesystem_size)
+    submission.decrement(:filesystem_size, filesystem_size)
+    submission.save!
   end
 
   private
--- db/migrate/20200322202625_add_file_size_to_submission.rb
+++ db/migrate/20200322202625_add_file_size_to_submission.rb
@@ -2,7 +2,7 @@ class AddFileSizeToSubmission < ActiveRecord::Migration
   def up
     add_column :submissions, :filesystem_size, :integer, default: 0
     SubmissionAsset.find_each do |submission_asset|
-      submission_asset.add_to_submission_filsize unless submission_asset.filesystem_size.nil?
+      submission_asset.add_to_submission_filesize unless submission_asset.filesystem_size.nil?
     end
   end
 

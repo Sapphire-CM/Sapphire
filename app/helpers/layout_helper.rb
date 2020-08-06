5254f867dbe0c36078e8e81a4092b34f5edc9410
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-08-06 20:27:13 +0200

    coding style improvements

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -138,7 +138,7 @@ module LayoutHelper
     render 'dropdown_button', title: title, dropdown_identifier: dropdown_identifier, links: links, dropdown_classes: dropdown_classes.join(' ')
   end
 
-  def markdown(text)
+  def format_markdown(text)
     return sanitize Kramdown::Document.new(text).to_html
   end
 end

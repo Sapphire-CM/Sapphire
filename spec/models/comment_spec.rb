f173b37b5aa5613ddc1c07f8ed47542ac2e4b493
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-08-27 00:18:31 +0200

    extended comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
   end
 
+  describe 'associations' do
+    it { is_expected.to belong_to(:commentable) }
+    it { is_expected.to belong_to(:account) }
+    it { is_expected.to belong_to(:term) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:account) }
+    it { is_expected.to validate_presence_of(:term) }
+    it { is_expected.to validate_presence_of(:content) }
+  end
 end

8fb1bcea8d8089c5b2c7412894f3268ff96ca38b
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-09-27 10:55:59 +0200

    init commenting feature spec
    This currently goes as far as having one test for editing a comment and
    the creation of comments

---
+++ spec/features/grading/staff/commenting_submission_evaluation_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe 'Commenting' do
+  let(:term) { FactoryGirl.create(:term) }
+  let(:account) { FactoryGirl.create(:account, :admin) }
+  let!(:term_registration) { FactoryGirl.create(:term_registration, :tutor, term: term, account: account) }
+  let!(:exercise) { FactoryGirl.create(:exercise, term: term)}
+  let(:submission) { FactoryGirl.create(:submission, exercise: exercise) }
+  let(:submission_evaluation) { submission.submission_evaluation }
+
+  before :each do
+    sign_in(account)
+  end
+
+
+  describe 'navigating' do
+    scenario 'to feedback comment modal', js: true do
+      visit (submission_evaluation_path(submission_evaluation))
+
+      within '.evaluation-top-bar' do
+        click_on text: 'Add Feedback'
+      end
+
+      expect(page).to have_css('#reveal_modal', class: 'open')
+    end
+  end
+
+  describe 'posting a comment' do
+    scenario 'with valid content', js: true do
+      visit (submission_evaluation_path(submission_evaluation))
+
+      within '.evaluation-top-bar' do
+        click_on text: 'Add Feedback'
+      end
+
+      expect do
+        within_modal do
+          fill_in 'Feedback', 'some comment'
+
+          click_button 'Save'
+        end
+
+        within '.comments_list' do
+          expect(page).to have_content("Feedback from #{account.fullname}")
+          expect(page).to have_content('some comment')
+        end
+      end
+
+      expect(page).to have_css('#reveal_modal', class: 'open')
+    end
+
+    scenario 'without valid content', js: true do
+      visit (submission_evaluation_path(submission_evaluation))
+
+      within '.evaluation-top-bar' do
+        click_on text: 'Add Feedback'
+      end
+
+      within_modal do
+        fill_in 'Feedback', with: ""
+
+        click_button 'Save'
+
+        expect(page).to have_content("can't be blank")
+      end
+
+      expect(page).to have_css('#reveal_modal', class: 'open')
+      expect(page).to have_content("can't be blank")
+    end
+  end
+
+  describe 'editing a comment' do
+    let!(:comment) { FactoryGirl.create :feedback_comment, commentable: submission_evaluation }
+    scenario 'to another valid comment', js: true do
+      visit (submission_evaluation_path(submission_evaluation))
+
+      within '.evaluation-top-bar' do
+        click_on text: 'Feedback (1)'
+      end
+
+      within_modal do
+        within '.comments_list' do
+          click_link href: edit_submission_evaluation_feedback_path(submission_evaluation, comment)
+
+          fill_in 'Feedback', with: 'this is an edited comment'
+
+          click_button 'Save'
+        end
+      end
+
+      expect(page).to have_content('this is an edited comment')
+    end
+  end
+end

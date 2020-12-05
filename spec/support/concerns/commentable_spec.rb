78678540dd8a982d800ef49c176db5759d45b550
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-12-05 17:15:22 +0100

    commentable concern now uses klass argument

--- spec/support/concerns/commentable_spec.rb
+++ spec/support/concerns/commentable_spec.rb
@@ -1,5 +1,5 @@
-RSpec.shared_examples 'a commentable' do |name|
-  let(:model) { FactoryGirl.create (described_class.to_s.underscore.to_sym) }
+RSpec.shared_examples 'a commentable' do |name, klass|
+  let(:model) { FactoryGirl.create (klass.to_s.underscore.to_sym) }
 
   it "has #{name} comments" do
     expect { model.send("#{name}_comments") }.to_not raise_error

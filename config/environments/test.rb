0b0dcf529cf9c738040280a339e7ecf5d6e34fc0
Matthias Link <matthee@users.noreply.github.com>
2021-11-11 11:03:41 +0100

    Add Dynamic action_mailer Configuration (#553)
    Introduce a mail.yml config file for configuring e-mail settings in different environments.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,11 +27,6 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
@@ -45,6 +40,4 @@ Rails.application.configure do
   # config.active_support.deprecation = :raise
 
   config.active_job.queue_adapter = :test
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :test
 end

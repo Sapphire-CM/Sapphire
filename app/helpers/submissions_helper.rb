0a045dec6434ba11651513869d44ed9afb6e9b8f
Duaa <duaa.nakshbandi@student.tugraz.at>
2022-09-04 19:17:50 +0200

    Seperate student name column into fore- and surname in the submissions view

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -51,6 +51,18 @@ module SubmissionsHelper
     end.join(", ").presence || content_tag(:em, 'unknown author')
   end
 
+  def submission_solitary_author_forename(submission)
+    submission.students.map do |student|
+      account_forname(student)
+    end.join(", ").presence || content_tag(:em, 'unknown author')
+  end
+
+  def submission_solitary_author_surname(submission)
+    submission.students.map do |student|
+      account_surname(student)
+    end.join(", ").presence || content_tag(:em, 'unknown author')
+  end
+
   def submission_group_author(submission)
     if submission.student_group_id?
       submission.student_group.title

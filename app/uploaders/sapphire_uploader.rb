c7c5872f40e60630ce1c441c354a6dbac2ff8230
Matthias Link <matthias@link.co.at>
2023-01-23 19:57:37 +0100

    Fix path joining

--- app/uploaders/sapphire_uploader.rb
+++ app/uploaders/sapphire_uploader.rb
@@ -2,11 +2,11 @@ class SapphireUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    Rails.root.join "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Rails.root.join "uploads", model.class.to_s.underscore, mounted_as.to_s, model.id.to_s
   end
 
   def cache_dir
-    Rails.root.join 'uploads/tmp'
+    Rails.root.join "uploads", "tmp"
   end
 
   def move_to_cache
